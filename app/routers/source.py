30843f31d1221d217dbc139a490c990be229677d
risto <risto.kowaczewski@gmail.com>
2021-04-04 00:27:06 +0200

    https://github.com/atompie/tracardi/issues/1#issue-849741839

--- app/routers/source.py
+++ app/routers/source.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+import elasticsearch
 from fastapi import APIRouter, Request
 from fastapi import HTTPException, Depends
 
@@ -16,28 +19,47 @@ router = APIRouter(
 
 @router.get("/{id}")
 def source_get(id: str, elastic=Depends(elastic_client)):
-    source_index = config.index['sources']
-    result = elastic.get(source_index, id)
-    return result['_source']
+    try:
+        sleep(1)
+        source_index = config.index['sources']
+        result = elastic.get(source_index, id)
+        return result['_source']
+    except NullResponseError as e:
+        raise HTTPException(status_code=e.response_status, detail={"message": str(e), "info": {}})
+
+    except elasticsearch.exceptions.NotFoundError as e:
+        raise HTTPException(status_code=e.status_code, detail={"message": "Record not found.", "info": e.info})
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"message": str(e), "info": {}})
 
 
 @router.post("/")
 def insert_source(source: Source, elastic=Depends(elastic_client)):
-    source_index = config.index['sources']
-    source = {
-        '_id': source.get_id(),
-        'itemId': source.get_id(),
-        'itemType': "source",
-        'name': source.name,
-        'hostname': source.hostname,
-        'metadata': {
-            'scope': source.scope,
-            'systemTags': [],
-            'tags': [],
-            'enabled': True
+    try:
+        source_index = config.index['sources']
+        source = {
+            '_id': source.get_id(),
+            'itemId': source.get_id(),
+            'itemType': "source",
+            'name': source.name,
+            'hostname': source.hostname,
+            'metadata': {
+                'scope': source.scope,
+                'systemTags': [],
+                'tags': [],
+                'enabled': True
+            }
         }
-    }
-    return elastic.insert(source_index, [source])
+        return elastic.insert(source_index, [source])
+    except NullResponseError as e:
+        raise HTTPException(status_code=e.response_status, detail={"message": str(e), "info": {}})
+
+    except elasticsearch.exceptions.NotFoundError as e:
+        raise HTTPException(status_code=e.status_code, detail={"message": "Record not found.", "info": e.info})
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"message": str(e), "info": {}})
 
 
 @router.post("/select")
@@ -71,6 +93,10 @@ async def source_select(request: Request, elastic=Depends(elastic_client)):
         return list(filter_source(result))
 
     except NullResponseError as e:
-        raise HTTPException(status_code=e.response_status, detail=str(e))
+        raise HTTPException(status_code=e.response_status, detail={"message": str(e), "info": {}})
+
+    except elasticsearch.exceptions.NotFoundError as e:
+        raise HTTPException(status_code=e.status_code, detail={"message": "Record not found.", "info": e.info})
+
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail={"message": str(e), "info": {}})
