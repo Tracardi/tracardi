bb4571a1b971934e71aaaa2c53ff914bb2e19510
risto <risto.kowaczewski@gmail.com>
2021-04-14 20:17:29 +0200

    tag 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tracardi_community',
-    version='0.2.0',
+    version='0.2.1',
     description='Tracardi for Unomi. Tracardi is GUI for Apache Unomi.',
     long_description=long_description,
     long_description_content_type="text/markdown",
