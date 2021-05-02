0d9a5c5fd2a997256d5b3d5c116c5b8daeb080e4
risto <risto.kowaczewski@gmail.com>
2021-05-02 21:32:55 +0200

    tests

--- app/tests/test_create_rule.py
+++ app/tests/test_create_rule.py
@@ -4,7 +4,7 @@ from ..uql.query.grammar.grammars import read
 from ..uql.query.transformers.create_rule_transformer import CreateRuleTransformer
 
 
-def test_create_rule():
+def test_regular_create_rule():
     p = Parser(read('uql_create_rule.lark'), start='create_rule')
     t = p.parse(
         """
@@ -20,7 +20,6 @@ def test_create_rule():
     )
 
     query = CreateRuleTransformer().transform(t)
-    # pprint(query)
 
     assert len(query) == 5
 
@@ -47,9 +46,149 @@ def test_create_rule():
     assert body['metadata']['missingPlugins'] == False
     assert body['metadata']['readOnly'] == False
     assert body['metadata']['scope'] == 'scope'
-    assert body['metadata']['tags'] == ['tag1','tag2']
+    assert body['metadata']['tags'] == ['tag1', 'tag2']
 
     assert body['condition']['type'] == 'eventPropertyCondition'
     assert body['condition']['parameterValues']['comparisonOperator'] == 'equals'
     assert body['condition']['parameterValues']['propertyName'] == 'eventType'
     assert body['condition']['parameterValues']['propertyValue'] == 'event'
+
+    assert body == {
+        "actions": [
+            {
+                "parameterValues": {
+                    "setPropertyName": "properties(a)",
+                    "setPropertyStrategy": "addValue",
+                    "setPropertyValue": "1"
+                },
+                "type": "setPropertyAction"
+            },
+            {
+                "parameterValues": {
+                    "setPropertyName": "properties(a)",
+                    "setPropertyStrategy": "alwaysSet",
+                    "setPropertyValue": "1"
+                },
+                "type": "setPropertyAction"
+            }
+        ],
+        "condition": {
+            "parameterValues": {
+                "comparisonOperator": "equals",
+                "propertyName": "eventType",
+                "propertyValue": "event"
+            },
+            "type": "eventPropertyCondition"
+        },
+        "itemId": "name",
+        "itemType": "rule",
+        "metadata": {
+            "description": "description",
+            "enabled": True,
+            "hidden": False,
+            "id": "name",
+            "missingPlugins": False,
+            "name": "name",
+            "readOnly": False,
+            "scope": "scope",
+            "tags": [
+                "tag1",
+                "tag2"
+            ]
+        },
+        "priority": -1,
+        "raiseEventOnlyOnceForProfile": False,
+        "raiseEventOnlyOnceForSession": False
+    }
+
+
+def test_boolean_create_rule():
+    p = Parser(read('uql_create_rule.lark'), start='create_rule')
+    t = p.parse(
+        """
+        CREATE RULE 
+        "name" 
+        DESCRIBE "description"
+        IN SCOPE "scope" 
+        WHEN event:type="identify" AND event:scope = "scope"  
+        THEN profile.CopyAll()
+        """
+    )
+
+    query = CreateRuleTransformer().transform(t)
+    pprint(query)
+
+    assert len(query) == 5
+
+    type, url, method, body, code = query
+
+    assert type == 'rule'
+    assert url == 'cxs/rules/'
+    assert method == 'POST'
+    assert code == 204
+
+    assert 'actions' in body
+    assert 'condition' in body
+    assert 'metadata' in body
+    assert 'itemId' in body
+    assert 'itemType' in body
+    assert 'priority' in body
+    assert 'raiseEventOnlyOnceForProfile' in body
+    assert 'raiseEventOnlyOnceForSession' in body
+
+    assert body['metadata']['description'] == 'description'
+    assert body['metadata']['enabled'] == True
+    assert body['metadata']['hidden'] == False
+    assert body['metadata']['id'] == 'name'
+    assert body['metadata']['missingPlugins'] == False
+    assert body['metadata']['readOnly'] == False
+    assert body['metadata']['scope'] == 'scope'
+
+    assert body == {
+        "actions": [
+            {
+                "parameterValues": {},
+                "type": "allEventToProfilePropertiesAction"
+            }
+        ],
+        "condition": {
+            "parameterValues": {
+                "operator": "and",
+                "subConditions": [
+                    {
+                        "parameterValues": {
+                            "comparisonOperator": "equals",
+                            "propertyName": "eventType",
+                            "propertyValue": "identify"
+                        },
+                        "type": "eventPropertyCondition"
+                    },
+                    {
+                        "parameterValues": {
+                            "comparisonOperator": "equals",
+                            "propertyName": "scope",
+                            "propertyValue": "scope"
+                        },
+                        "type": "eventPropertyCondition"
+                    }
+                ]
+            },
+            "type": "booleanCondition"
+        },
+        "itemId": "name",
+        "itemType": "rule",
+        "metadata": {
+            "description": "description",
+            "enabled": True,
+            "hidden": False,
+            "id": "name",
+            "missingPlugins": False,
+            "name": "name",
+            "readOnly": False,
+            "scope": "scope",
+            "tags": []
+        },
+        "priority": -1,
+        "raiseEventOnlyOnceForProfile": False,
+        "raiseEventOnlyOnceForSession": False
+    }
