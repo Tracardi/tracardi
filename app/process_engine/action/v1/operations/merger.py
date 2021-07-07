825b75c8ea220f0f0a47ccf6852bf6683b11eb16
risto <risto.kowaczewski@gmail.com>
2021-07-07 02:45:10 +0200

    merger

--- app/process_engine/action/v1/operations/merger.py
+++ app/process_engine/action/v1/operations/merger.py
@@ -1,32 +1,52 @@
-candidates = dict()
-path = []
+def validate_list_values(values):
+    for value in values:
+        if type(value) not in [str, int, float, bool]:
+            raise ValueError("Invalid value in list `{}`".format(values))
 
 
 def append(base, key, value):
     if not isinstance(base, dict):
-        raise ValueError("Expected dict but got {}".format(base))
+        raise ValueError("Could not merge value `{}` and values `{}`".format(
+            base,
+            {key: value}))
+
+    # Treat tuple and set as list
+    if key in base:
+        if type(base[key]) in [tuple, set]:
+            base[key] = list(base[key])
+        if isinstance(base[key], list):
+            validate_list_values(base[key])
+
+    if type(value) in [set, tuple]:
+        value = list(value)
+    if isinstance(value, list):
+        validate_list_values(value)
+
+    # Merge
 
     if key not in base:
         base[key] = value
     else:
-        # treat set as list
-        if isinstance(base[key], set):
-            base[key] = list(base[key])
+        """
+        Existing value can be only a list or a simple type
+        """
 
-        if isinstance(value, set):
-            value = list(value)
-
+        # Existing value is a list
         if isinstance(base[key], list):
             if isinstance(value, list):
                 base[key] += value
             else:
                 base[key].append(value)
-        else:
+            # Existing value is not a dict and value is a simple value
+        elif not isinstance(base[key], dict) and type(value) in [str, int, float, bool, list]:
             base[key] = [base[key]]
             if isinstance(value, list):
+                validate_list_values(value)
                 base[key] += value
             else:
                 base[key].append(value)
+        else:
+            raise ValueError("Could not merge `{}` with value `{}`".format(base[key], value))
 
     # make uniq
     if isinstance(base[key], list):
@@ -39,27 +59,29 @@ def merge(base, dict_list):
     for key in set().union(*dict_list):
         for data in dict_list:
             if key in data:
-                if type(data[key]) in [str, int, float, list, set]:
-                    if not isinstance(base, dict):
-                        raise ValueError("Could not merge value `{}` and values `{}`".format(base, dict_list))
+                # Null
+                if data[key] is None:
+                    continue
+                # Simple types
+                elif type(data[key]) in [str, int, float, bool, tuple, list, set]:
                     append(base, key, data[key])
+                # Dicts
                 elif type(data[key]) in [dict]:
                     if key not in base:
                         base[key] = {}
                     base[key] = merge(base[key], [data[key]])
+                # Objects
+                elif isinstance(data[key], object):
+                    raise ValueError("Object of type `{}: {}` can not be merged with value `{}: {}`".format(
+                        key, type(data[key]),
+                        key, base[key] if key in base else base))
                 else:
-                    print(key, data[key])
+                    raise ValueError("Unknown type `{}: {}`".format(key, type(data[key])))
+
     return base
 
 
-# def merge_candidates(dict_list):
-#     return {
-#       k: [d.get(k) for d in dict_list if k in d] # explanation A
-#       for k in set().union(*dict_list) # explanation B
-#     }
-
 if __name__ == "__main__":
-
     a = {
         "a": 1,
         "b": [1, 2],
@@ -83,7 +105,7 @@ if __name__ == "__main__":
         "conflict": "a",
     }
 
-    c = merge(candidates, [a, b])
+    c = merge({}, [a, b])
     # d = merge_candidates([a, b])
     print(c)
     # print(d)
--- app/tests/unit/test_merger.py
+++ app/tests/unit/test_merger.py
@@ -1,4 +1,15 @@
+from datetime import datetime
+
 from app.process_engine.action.v1.operations.merger import merge
+
+
+def test_merger_multiple_values():
+    a = {"a": None}
+    b = {"a": .2}
+    c = {"a": [1]}
+    d = {"a": "3"}
+    m = merge({}, [a, b, c, d])
+    assert set(m['a']).intersection({.2, 1, "3"}) == {.2, 1, "3"}
 
 
 def test_merger_none_values():
@@ -51,16 +62,40 @@ def test_merger_single_values():
     assert c == {"a": [.1, .2]} or c == {"a": [.2, .1]}
 
 
+def test_merger_bool_values():
+    a = {"a": True}
+    b = {"a": False, "b": True}
+    c = merge({}, [a, b])
+    assert c == {"a": [False, True], "b": True}
+
+    a = {"a": True}
+    b = {"a": [False], "b": True}
+    c = merge({}, [a, b])
+    assert c == {"a": [False, True], "b": True}
+
+    a = {"a": [True]}
+    b = {"a": False, "b": True}
+    c = merge({}, [a, b])
+    assert c == {"a": [False, True], "b": True}
+
+    a = {"a": [True]}
+    b = {"a": (False,) , "b": True}
+    c = merge({}, [a, b])
+    assert c == {"a": [False, True], "b": True}
+
+
 def test_merger_single_value_plus_list():
     # mixed
 
     a = {"a": [1]}
     b = {"a": "2"}
-    assert merge({}, [a, b]) == {"a": [1, "2"]}
+    c = merge({}, [a, b])
+    assert c == {"a": [1, "2"]} or c == {"a": ["2", 1]}
 
     a = {"a": 1}
     b = {"a": ["2"]}
-    assert merge({}, [a, b]) == {"a": [1, "2"]}
+    c = merge({}, [a, b])
+    assert c == {"a": [1, "2"]} or c == {"a": ["2", 1]}
 
     # string
 
@@ -79,22 +114,24 @@ def test_merger_single_value_plus_list():
     a = {"a": [1]}
     b = {"a": 2}
     c = merge({}, [a, b])
-    assert c == {"a": [1, 2]}
+    assert c == {"a": [1, 2]} or c == {"a": [2, 1]}
 
     a = {"a": 1}
     b = {"a": [2]}
     c = merge({}, [a, b])
-    assert c == {"a": [1, 2]}
+    assert c == {"a": [1, 2]} or c == {"a": [2, 1]}
 
     # float
 
     a = {"a": [.1]}
     b = {"a": .2}
-    assert merge({}, [a, b]) == {"a": [.1, .2]}
+    c = merge({}, [a, b])
+    assert c == {"a": [.1, .2]} or c == {"a": [.2, .1]}
 
     a = {"a": .1}
     b = {"a": [.2]}
-    assert merge({}, [a, b]) == {"a": [.1, .2]}
+    c = merge({}, [a, b])
+    assert c == {"a": [.1, .2]} or c == {"a": [.2, .1]}
 
 
 def test_merger_uniq():
@@ -139,12 +176,12 @@ def test_merger_dict_list_2_list():
     a = {"a": {"b": [1, 2]}}
     b = {"a": {"b": [3, 4]}}
     c = merge({}, [a, b])
-    assert c == {"a": {"b": [1, 2, 3, 4]}}
+    assert "b" in c['a'] and set(c['a']['b']).intersection({1, 2, 3, 4}) == {1, 2, 3, 4}
 
     a = {"a": [1, 2, 3]}
     b = {"a": [3, 4, 5]}
     c = merge({}, [a, b])
-    assert c == {"a": [1, 2, 3, 4, 5]}
+    assert set(c['a']).intersection({1, 2, 3, 4, 5}) == {1, 2, 3, 4, 5}
 
 
 def test_merger_dict_list_2_set():
@@ -176,38 +213,58 @@ def test_merger_dict_set_2_set():
     assert c == {"a": [1, 2, 3, 4, 5]}
 
 
+def test_merger_tuple_2_list():
+    """
+    Merges dict with value int, float, str
+    """
+
+    a = {"a": 1}
+    b = {"a": (2, 3)}
+    c = merge({}, [a, b])
+    assert c == {"a": [1, 2, 3]}
+
+    a = {"a": [1]}
+    b = {"a": (2, 3)}
+    c = merge({}, [a, b])
+    assert c == {"a": [1, 2, 3]}
+
+
 def test_merger_dict_single_list_values():
     # mixed dict
 
     a = {"a": {"b": [1]}}
     b = {"a": {"b": "2"}}
-    assert merge({}, [a, b]) == {"a": {"b": [1, "2"]}}
+    c = merge({}, [a, b])
+    assert c == {"a": {"b": [1, "2"]}} or c == {"a": {"b": ["2", 1]}}
 
     a = {"a": {"b": 1}}
     b = {"a": {"b": ["2"]}}
-    assert merge({}, [a, b]) == {"a": {"b": [1, "2"]}}
+    c = merge({}, [a, b])
+    assert c == {"a": {"b": [1, "2"]}} or {"a": {"b": ["2", 1]}}
 
     # int
 
     a = {"a": {"b": [1]}}
     b = {"a": {"b": 2}}
     c = merge({}, [a, b])
-    assert c == {"a": {"b": [1, 2]}}
+    assert c == {"a": {"b": [1, 2]}} or c == {"a": {"b": [2, 1]}}
 
     a = {"a": {"b": 1}}
     b = {"a": {"b": [2]}}
     c = merge({}, [a, b])
-    assert c == {"a": {"b": [1, 2]}}
+    assert c == {"a": {"b": [1, 2]}} or c == {"a": {"b": [2, 1]}}
 
     # float
 
     a = {"a": {"b": [.1]}}
     b = {"a": {"b": .2}}
-    assert merge({}, [a, b]) == {"a": {"b": [.1, .2]}}
+    c = merge({}, [a, b])
+    assert c == {"a": {"b": [.1, .2]}} or c == {"a": {"b": [.2, .1]}}
 
     a = {"a": {"b": .1}}
     b = {"a": {"b": [.2]}}
-    assert merge({}, [a, b]) == {"a": {"b": [.1, .2]}}
+    c = merge({}, [a, b])
+    assert c == {"a": {"b": [.1, .2]}} or c == {"a": {"b": [.2, .1]}}
 
     # string
 
@@ -223,12 +280,88 @@ def test_merger_dict_single_list_values():
 
 
 def test_merger_conflict_1():
+    """
+    Merges dict with value int, float, str
+    """
+
     a = {"a": 1}
     b = {"a": {"b": 1}}
-    c = merge({}, [a, b])
-    assert c == {"a": {"b": [1, 2, 3, 4]}}
 
-    # a = {"a": {"b": 1}}
-    # b = {"a": 1}
-    # c = merge({}, [a, b])
-    # assert c == {"a": {"b": [1, 2, 3, 4]}}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+    a = {"a": {"b": 1}}
+    b = {"a": 1}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+
+def test_merger_conflict_2():
+    """
+    Merges dict with value int, float, str
+    """
+
+    a = {"a": 1}
+    b = {"a": datetime.now()}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+
+def test_merger_conflict_3():
+    """
+    Conflict inside list of values
+    """
+
+    a = {"a": 1}
+    b = {"a": [2, [3]]}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+    a = {"a": [2, [3]]}
+    b = {"a": 1}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+    a = {"a": (2, [3])}
+    b = {"a": 1}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+    a = {"a": 1}
+    b = {"a": (2, [3])}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+
+def test_merger_conflict_4():
+    """
+    Conflict inside list of values
+    """
+
+    a = {"a": datetime.now}
+    b = {"a": [2, 3]}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
+
+    a = {"a": datetime.now()}
+    b = {"a": [2, 3]}
+    try:
+        merge({}, [a, b])
+    except ValueError:
+        assert True
