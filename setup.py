9be1df20478b1e1b5a95614764010bc62d598a64
risto <risto.kowaczewski@gmail.com>
2021-08-09 02:19:25 +0200

    https://github.com/atompie/tracardi/issues/49
    Removes API

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='tracardi',
-    version='0.5.0',
+    version='0.5.1',
     description='Trackardi Customer Data Platform backend',
     long_description=long_description,
     long_description_content_type="text/markdown",
--- tracardi/config.py
+++ tracardi/config.py
@@ -3,8 +3,6 @@ import os
 
 class TracardiConfig:
     def __init__(self, env):
-        self.user = env['USER_NAME'] if 'USER_NAME' in env else 'admin'
-        self.password = env['PASSWORD'] if 'PASSWORD' in env else 'admin'
         self.track_debug = env['TRACK_DEBUG'] if 'TRACK_DEBUG' in env else False
 
 
