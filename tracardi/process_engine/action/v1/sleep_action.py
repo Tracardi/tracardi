a2cab9b9750f62d14dce12320bfb11076b03d0fe
risto <risto.kowaczewski@gmail.com>
2021-08-16 15:44:42 +0200

    sleep action

---
+++ tracardi/process_engine/action/v1/sleep_action.py
@@ -0,0 +1,41 @@
+from asyncio import sleep
+
+from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData
+from tracardi_plugin_sdk.action_runner import ActionRunner
+from tracardi_plugin_sdk.domain.result import Result
+
+
+class SleepAction(ActionRunner):
+
+    def __init__(self, **kwargs):
+        self.sleep = kwargs['sleep'] if 'sleep' in kwargs else 0
+
+    async def run(self, payload):
+        await sleep(self.sleep)
+        return Result(port="void", value=payload)
+
+
+def register() -> Plugin:
+    return Plugin(
+        start=False,
+        spec=Spec(
+            module='tracardi.process_engine.action.v1.sleep_action',
+            className='SleepAction',
+            inputs=["payload"],
+            outputs=["payload"],
+            version='0.1.1',
+            license="MIT",
+            author="Risto Kowaczewski",
+            init={"sleep": 1}
+
+        ),
+        metadata=MetaData(
+            name='Sleep',
+            desc='Sleeps workflow.',
+            type='flowNode',
+            width=100,
+            height=100,
+            icon='stop',
+            group=["Input/Output"]
+        )
+    )
