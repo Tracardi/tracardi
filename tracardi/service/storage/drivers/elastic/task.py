ec0c6ba3ba3c5976d108dca8f3f16e1cb462e11b
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2021-11-07 10:23:34 +0100

    fixed add_scheduled_event_endpoint (#128)
    * fixed add_scheduled_event_endpoint
    
    * fixed SRP

--- tracardi/service/storage/drivers/elastic/task.py
+++ tracardi/service/storage/drivers/elastic/task.py
@@ -1,6 +1,8 @@
 import time
 
 from tracardi.service.storage.factory import storage_manager, StorageForBulk
+from uuid import uuid4
+from datetime import datetime
 
 
 async def load_pending_tasks(limit: int = 100):
@@ -37,3 +39,30 @@ async def save_tasks(tasks):
 
 async def load_all(start: int = 0, limit: int = 100):
     return await storage_manager('task').load_all(start, limit)
+
+
+async def create(timestamp, type, properties, context, session_id, source_id, profile_id, status):
+    return await storage_manager('task').upsert(data=[{
+        "id": uuid4(),
+        "timestamp": timestamp,
+        "event": {
+            "metadata": {
+                "time": {
+                    "insert": datetime.utcnow()
+                }
+            },
+            "type": type,
+            "properties": properties,
+            "context": context,
+            "session": {
+                "id": session_id
+            },
+            "source": {
+                "id": source_id
+            },
+            "profile": {
+                "id": profile_id
+            }
+        },
+        "status": status
+    }], replace_id=True)
