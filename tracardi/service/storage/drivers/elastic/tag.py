103b4dd0d0cc9474aa3c31838398b99a904a561f
DawidekZagajnik <krukdawid27@gmail.com>
2021-11-08 18:47:43 +0100

    tag driver refactoring

--- tracardi/service/storage/drivers/elastic/tag.py
+++ tracardi/service/storage/drivers/elastic/tag.py
@@ -8,16 +8,17 @@ async def get_document_by_event_type(event_type):
 
 async def add_tags(event_type: str, tags: List[str]):
     search_result = await get_document_by_event_type(event_type=event_type)
-    if search_result.dict()["result"]:
-        tags.extend(search_result.dict()["result"][0]["tags"])
+    result_dict = search_result.dict()
+    if result_dict["result"]:
+        tags.extend(result_dict["result"][0]["tags"])
         tags = list(set(tags))
-        return 1, await storage_manager("event-tags").upsert({
-            "_id": search_result.dict()["result"][0]["id"],
-            "type": search_result.dict()["result"][0]["type"],
+        return await storage_manager("event-tags").upsert({
+            "_id": result_dict["result"][0]["id"],
+            "type": result_dict["result"][0]["type"],
             "tags": tags
         })
     else:
-        return 0, await storage_manager("event-tags").upsert({
+        return await storage_manager("event-tags").upsert({
             "type": event_type,
             "tags": tags
         })
@@ -25,17 +26,18 @@ async def add_tags(event_type: str, tags: List[str]):
 
 async def remove_tags(event_type: str, tags: List[str]):
     search_result = await get_document_by_event_type(event_type=event_type)
-    if search_result.dict()['result']:
-        if set(tags).issubset(set(search_result.dict()["result"][0]["tags"])):
+    result_dict = search_result.dict()
+    if result_dict['result']:
+        if set(tags).issubset(set(result_dict["result"][0]["tags"])):
             return 0, \
-                   len(search_result.dict()["result"][0]["tags"]), \
+                   len(result_dict["result"][0]["tags"]), \
                    await storage_manager("event-tags").delete_by(field="type", value=event_type)
         else:
-            tags_to_insert = [tag for tag in search_result.dict()["result"][0]["tags"] if tag not in tags]
+            tags_to_insert = [tag for tag in result_dict["result"][0]["tags"] if tag not in tags]
             return len(tags_to_insert), \
-                len(search_result.dict()["result"][0]["tags"]) - len(tags_to_insert), \
+                len(result_dict["result"][0]["tags"]) - len(tags_to_insert), \
                 await storage_manager("event-tags").upsert({
-                    "_id": search_result.dict()["result"][0]["id"],
+                    "_id": result_dict["result"][0]["id"],
                     "type": event_type,
                     "tags": tags_to_insert
                 })
@@ -43,5 +45,5 @@ async def remove_tags(event_type: str, tags: List[str]):
         raise ValueError("There is no document with 'type' field equal to '{}'.".format(event_type))
 
 
-async def load_all_tags(limit):
-    return storage_manager("event-tags").load_all(start=0, limit=limit)
+async def load_all_tags(limit: int = 100):
+    return await storage_manager("event-tags").load_all(start=0, limit=limit)
