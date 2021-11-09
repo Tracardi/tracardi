2009113a6392b85c859004df65ed380fb96acd8f
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2021-11-09 18:32:28 +0100

    Delete tags_service.py

--- tracardi/service/tags_service.py
+++
@@ -1,4 +0,0 @@
-from tracardi.event_server.utils.memory_cache import MemoryCache
-
-# musi wczytywać do pamięci cache ustawienia z indeksu tag, użyj MemoryCache,
-# service musi wyciągać event z bazy i zwracać go z wypełnionym "tags"
