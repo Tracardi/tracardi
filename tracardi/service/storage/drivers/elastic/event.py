a26aae1f537a22a0d8a7aa4c28a8af6cd9bbb738
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2021-11-11 17:00:41 +0100

    Update event.py

--- tracardi/service/storage/drivers/elastic/event.py
+++ tracardi/service/storage/drivers/elastic/event.py
@@ -222,7 +222,7 @@ async def load_events_heatmap(profile_id: str):
     return list(convert_data(raw_result))
 
 
-async def update_fields(event_type: str, field: str, value: Any):
+async def update_field(event_type: str, field: str, value: Any):
     query = {
         "script": {
             "source": f"ctx._source.{field} = {value};",
