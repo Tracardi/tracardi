4b7ccdba842027d1c442d60b2812b21f7702df47
DawidekZagajnik <krukdawid27@gmail.com>
2021-11-11 14:05:28 +0100

    exception fix

--- tracardi/service/tags_service.py
+++ tracardi/service/tags_service.py
@@ -1,4 +1,16 @@
-from tracardi.event_server.utils.memory_cache import MemoryCache
+from tracardi.event_server.utils.memory_cache import CacheItem
+from tracardi.domain.event import Event
+from tracardi.service.storage.driver import storage
+from tracardi.config import memory_cache
+from tracardi.domain.event_tag import EventTag
 
-# musi wczytywać do pamięci cache ustawienia z indeksu tag, użyj MemoryCache,
-# service musi wyciągać event z bazy i zwracać go z wypełnionym "tags"
+
+def tags_service(event: Event):
+    if "tags-type-{}".format(event.type) not in memory_cache:
+        result = list(await storage.driver.tag.get_by_type(event.type)).pop()
+        memory_cache["tags-type-{}".format(event.type)] = CacheItem(
+            data=EventTag(**result).tags,
+            ttl=memory_cache.tags_ttl
+        )
+    event.tags = memory_cache["tags-type-{}".format(event.type)].data
+    return event
