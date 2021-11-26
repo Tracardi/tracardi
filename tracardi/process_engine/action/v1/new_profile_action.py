7cec06a3a662af375f53c8dce1fd0e827290852c
risto <risto.kowaczewski@gmail.com>
2021-11-27 00:19:44 +0100

    Port documentation.

--- tracardi/process_engine/action/v1/new_profile_action.py
+++ tracardi/process_engine/action/v1/new_profile_action.py
@@ -1,4 +1,4 @@
-from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData
+from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData, Documentation, PortDoc
 from tracardi_plugin_sdk.action_runner import ActionRunner
 from tracardi_plugin_sdk.domain.result import Result
 
@@ -10,9 +10,9 @@ class NewProfileAction(ActionRunner):
 
     async def run(self, payload):
         if self.profile.operation.new:
-            return Result(port="TRUE", value=payload)
+            return Result(port="true", value=payload)
 
-        return Result(port="FALSE", value=payload)
+        return Result(port="false", value=payload)
 
 
 def register() -> Plugin:
@@ -22,7 +22,7 @@ def register() -> Plugin:
             module='tracardi.process_engine.action.v1.new_profile_action',
             className='NewProfileAction',
             inputs=["payload"],
-            outputs=['TRUE', 'FALSE'],
+            outputs=['true', 'false'],
             init=None,
             manual="new_profile_action",
             version='0.1',
@@ -34,6 +34,15 @@ def register() -> Plugin:
             desc='If new profile then it returns true on TRUE output, otherwise returns false on FALSE port.',
             keywords=['condition'],
             type='flowNode',
+            documentation=Documentation(
+                inputs={
+                    "payload": PortDoc(desc="This port takes any JSON like object.")
+                },
+                outputs={
+                    "true": PortDoc(desc="This port returns payload if the defined condition is met."),
+                    "false": PortDoc(desc="This port returns payload if the defined condition is NOT met.")
+                }
+            ),
             width=200,
             height=100,
             icon='question',
