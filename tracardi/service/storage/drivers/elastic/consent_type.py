dad34795365efbe5cd84ffe58cde8b692ce62e38
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2021-11-30 19:35:08 +0100

    quick fix of consent_type driver for consent plugin to work + bug fix (#234)
    * quick fix for consent plugin to work

--- tracardi/service/storage/drivers/elastic/consent_type.py
+++ tracardi/service/storage/drivers/elastic/consent_type.py
@@ -1,9 +1,12 @@
 from tracardi.domain.storage_result import StorageResult
 from tracardi.service.storage.factory import storage_manager
-from uuid import UUID
 
 
-async def add_consent(id: UUID, name: str, description: str, revokable: bool, default_value: str):
+async def get_by_id(consent_id):
+    return await storage_manager("consent-type").load(consent_id)
+
+
+async def add_consent(id: str, name: str, description: str, revokable: bool, default_value: str):
     return await storage_manager("consent-type").upsert(data={
         "id": id,
         "name": name,
@@ -19,3 +22,4 @@ async def delete_by_id(consent_id):
 
 async def load(start: int = 0, limit: int = 10) -> StorageResult:
     return await storage_manager("consent-type").load_all(start=start, limit=limit)
+
