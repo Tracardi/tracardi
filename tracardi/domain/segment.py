d77f5abde68daeaf7a387fdb4d962627a8fa436d
risto <risto.kowaczewski@gmail.com>
2021-12-08 20:26:15 +0100

    https://github.com/Tracardi/tracardi-api/pull/26

--- tracardi/domain/segment.py
+++ tracardi/domain/segment.py
@@ -1,8 +1,6 @@
-from typing import Optional
-
+from typing import Optional, Any
 from tracardi.domain.named_entity import NamedEntity
 from tracardi.domain.value_object.storage_info import StorageInfo
-from pydantic import root_validator
 
 
 class Segment(NamedEntity):
@@ -10,14 +8,14 @@ class Segment(NamedEntity):
     eventType: Optional[str] = None
     condition: str
     enabled: bool = True
+    machine_name: str = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.machine_name = self.get_id()
 
     def get_id(self) -> str:
         return self.name.replace(" ", "-").lower()
-
-    @root_validator
-    def machine_name(cls, values):
-        values["machine_name"] = values["name"].replace(" ", "-").lower()
-        return values
 
     @staticmethod
     def storage_info() -> StorageInfo:
