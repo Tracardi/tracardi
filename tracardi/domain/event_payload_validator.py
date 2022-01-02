bd946db168251397d31bc96c9ae49e3eaa6f1201
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-01-02 20:48:44 +0100

    Consent type and validation schema fix (#317)

--- tracardi/domain/event_payload_validator.py
+++ tracardi/domain/event_payload_validator.py
@@ -2,11 +2,15 @@ import jsonschema
 from pydantic import BaseModel, validator
 from typing import Dict
 from tracardi.service.secrets import encrypt, decrypt
+from typing import Optional
 
 
 class EventPayloadValidator(BaseModel):
     validation: Dict[str, Dict]
     event_type: str
+    name: str
+    description: Optional[str] = "No description provided"
+    enabled: bool
 
     @validator("validation")
     def validate_schemas_format(cls, v):
@@ -20,19 +24,28 @@ class EventPayloadValidator(BaseModel):
     def encode(self) -> 'EventPayloadValidatorRecord':
         return EventPayloadValidatorRecord(
             validation=encrypt(self.validation),
-            id=self.event_type.lower().replace(" ", "-")
+            id=self.event_type.lower().replace(" ", "-"),
+            name=self.name,
+            description=self.description,
+            enabled=self.enabled
         )
 
     @staticmethod
     def decode(record: 'EventPayloadValidatorRecord') -> 'EventPayloadValidator':
         return EventPayloadValidator(
             validation=decrypt(record.validation),
-            event_type=record.id
+            event_type=record.id,
+            name=record.name,
+            description=record.description,
+            enabled=record.enabled
         )
 
 
 class EventPayloadValidatorRecord(BaseModel):
     validation: str
     id: str
+    name: str
+    description: str
+    enabled: bool
 
 
