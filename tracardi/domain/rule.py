1677254b5a5f19aad21a9b9e030f8b475a708885
risto <risto.kowaczewski@gmail.com>
2022-01-04 13:34:31 +0100

    Rules.

--- tracardi/domain/rule.py
+++ tracardi/domain/rule.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-from typing import Optional, Any
+from typing import Optional, Any, List
+
+from pydantic import validator
+
 from .metadata import Metadata
 from .named_entity import NamedEntity
 from .time import Time
@@ -15,6 +18,13 @@ class Rule(NamedEntity):
     description: Optional[str] = "No description provided"
     properties: Optional[dict] = None
     metadata: Optional[Metadata]
+    tags: Optional[List[str]] = ["General"]
+
+    @validator("tags")
+    def tags_can_not_be_empty(cls, value):
+        if len(value) == 0:
+            value = ["General"]
+        return value
 
     def __init__(self, **data: Any):
         data['metadata'] = Metadata(
