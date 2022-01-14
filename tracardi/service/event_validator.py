52c9c8f554ad5366ee76e0e662e6ed6f2cb80c5d
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-01-14 09:56:40 +0100

    Validation fix (#334) (#343)
    * validation fix

--- tracardi/service/event_validator.py
+++ tracardi/service/event_validator.py
@@ -2,11 +2,11 @@ from tracardi.service.notation.dot_accessor import DotAccessor
 import jsonschema
 from tracardi.domain.event_payload_validator import EventPayloadValidator
 from tracardi.exceptions.exception import EventValidationException
-
+from dotty_dict import Dotty
 
 def validate(dot: DotAccessor, validator: EventPayloadValidator) -> None:
     for key, val_schema in validator.validation.items():
         try:
-            jsonschema.validate(dot[key], val_schema)
+            jsonschema.validate(dot[key].to_dict() if isinstance(dot[key], Dotty) else dot[key], val_schema)
         except (jsonschema.ValidationError, KeyError) as e:
             raise EventValidationException("{}: {}".format(key, str(e)))
