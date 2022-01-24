272d8f3f5726e3062247b29a9413b22723fce102
risto <risto.kowaczewski@gmail.com>
2022-01-24 19:46:06 +0100

    CTA message plugin

--- tracardi/process_engine/action/v1/ux/cta_message/plugin.py
+++ tracardi/process_engine/action/v1/ux/cta_message/plugin.py
@@ -7,12 +7,19 @@ from tracardi.service.plugin.domain.result import Result
 
 
 class Configuration(BaseModel):
-    type: str = "success"
-    message: str
-    hide_after: str
-    position_x: str
-    position_y: str
     uix_mf_source: AnyHttpUrl = "http://localhost:8686"
+    title: str = ""
+    message: str = ""
+    cta_button: str = ""
+    cta_link: str = ""
+    cancel_button: str = ""
+    border_radius: int = 2
+    border_shadow: int = 1
+    min_width: int = 300
+    max_width: int = 500,
+    hide_after: int = 6000
+    position_x: str = "right"
+    position_y: str = "bottom"
 
     @validator("message")
     def should_no_be_empty(cls, value):
@@ -20,10 +27,10 @@ class Configuration(BaseModel):
             raise ValueError("Message should not be empty")
         return value
 
-    @validator("hide_after")
-    def hide_after_should_be_numeric(cls, value: str):
-        if not value.isnumeric():
-            raise ValueError("This value should be numeric")
+    @validator("cta_button")
+    def cta_no_be_empty(cls, value):
+        if len(value) == 0:
+            raise ValueError("CTA should not be empty")
         return value
 
 
@@ -31,7 +38,7 @@ def validate(config: dict):
     return Configuration(**config)
 
 
-class SnackBarUx(ActionRunner):
+class CtaMessageUx(ActionRunner):
 
     def __init__(self, **kwargs):
         self.config = validate(kwargs)
@@ -40,15 +47,22 @@ class SnackBarUx(ActionRunner):
         self.ux.append({
             "tag": "div",
             "props": {
-                "class": "tracardi-uix-snackbar",
-                "data-type": self.config.type,
+                "class": "tracardi-uix-cta-message",
+                "data-title": self.config.title,
                 "data-message": self.config.message,
                 "data-vertical": self.config.position_y,
                 "data-horizontal": self.config.position_x,
-                "data-auto-hide": self.config.hide_after
+                "data-auto-hide": self.config.hide_after,
+                "data-cta-button": self.config.cta_button,
+                "data-cta-link": self.config.cta_link,
+                "data-cancel-button": self.config.cancel_button,
+                "data-border-radius": self.config.border_radius,
+                "data-border-shadow": self.config.border_shadow,
+                "data-min-width": self.config.min_width,
+                "data-max-width": self.config.max_width
             }
         })
-        self.ux.append({"tag": "script", "props": {"src": f"{self.config.uix_mf_source}/uix/snackbar/index.js"}})
+        self.ux.append({"tag": "script", "props": {"src": f"{self.config.uix_mf_source}/uix/cta-message/index.js"}})
 
         return Result(port="payload", value=payload)
 
@@ -58,14 +72,21 @@ def register() -> Plugin:
         start=False,
         spec=Spec(
             module=__name__,
-            className='SnackBarUx',
+            className='CtaMessageUx',
             inputs=["payload"],
             outputs=["payload"],
             init={
-                "type": "success",
+                "title": "",
                 "message": "",
+                "cta_button": "",
+                "cta_link": "",
+                "cancel_button": "",
+                "border_radius": 2,
+                "border_shadow": 1,
+                "min_width": 300,
+                "max_width": 500,
                 "hide_after": 6000,
-                "position_x": "center",
+                "position_x": "right",
                 "position_y": "bottom",
                 "uix_mf_source": "http://localhost:8686"
             },
@@ -77,28 +98,45 @@ def register() -> Plugin:
                     name="Widget Message Configuration",
                     fields=[
                         FormField(
+                            id="title",
+                            name="Title",
+                            description="Provide title. Title can be empty the it will no be displayed.",
+                            component=FormComponent(type="text", props={"label": "title"})
+                        ),
+                        FormField(
                             id="message",
-                            name="Pop-up message",
-                            description="Provide message that will be shown on the web page.",
+                            name="Message before CTA button",
+                            description="Provide message that will be displayed before CTA button.",
                             component=FormComponent(type="textarea", props={"label": "message"})
                         ),
                         FormField(
-                            id="type",
-                            name="Alert type",
-                            description="Select alert type.",
-                            component=FormComponent(type="select", props={"label": "Alert type", "items": {
-                                "error": "Error",
-                                "warning": "Warning",
-                                "success": "Success",
-                                "info": "Info"
-                            }})
+                            id="cta_button",
+                            name="CTA button text",
+                            description="Provide text to be displayed o CTA button.",
+                            component=FormComponent(type="text", props={"label": "CTA button text"})
+                        ),
+                        FormField(
+                            id="cta_link",
+                            name="CTA link",
+                            description="Provide CTA button link.",
+                            component=FormComponent(type="text", props={"label": "CTA link"})
+                        ),
+                        FormField(
+                            id="cancel_button",
+                            name="Cancel button text",
+                            description="Provide CANCEL button text. If empty the button will not be displayed.",
+                            component=FormComponent(type="text", props={"label": "Cancel text"})
                         ),
                         FormField(
                             id="hide_after",
                             name="Hide message after",
                             description="Type number of milliseconds the message must be visible. Default: 6000. 6sec.",
                             component=FormComponent(type="text", props={"label": "hide after"})
-                        ),
+                        )
+                    ]),
+                FormGroup(
+                    name="Widget Position and Width",
+                    fields=[
                         FormField(
                             id="position_y",
                             name="Vertical position",
@@ -115,9 +153,21 @@ def register() -> Plugin:
                             component=FormComponent(type="select", props={"label": "Horizontal position", "items": {
                                 "left": "Left",
                                 "center": "Center",
-                                "Right": "Right"
+                                "right": "Right"
                             }})
                         ),
+                        FormField(
+                            id="min_width",
+                            name="Minimal width",
+                            description="Minimal width of the pop-up window.",
+                            component=FormComponent(type="text", props={"label": "minimal width"})
+                        ),
+                        FormField(
+                            id="max_width",
+                            name="Maximal width",
+                            description="Maximal width of the pop-up window.",
+                            component=FormComponent(type="text", props={"label": "maximal width"})
+                        )
                     ]),
                 FormGroup(
                     name="Widget Source Location",
@@ -135,8 +185,8 @@ def register() -> Plugin:
 
         ),
         metadata=MetaData(
-            name='Show snack bar',
-            desc='Shows snack bar pop-up on the front end.',
+            name='CTA message',
+            desc='Shows massage CTA button pop-up on the front end.',
             icon='react',
             group=["UIX Widgets"],
             documentation=Documentation(
