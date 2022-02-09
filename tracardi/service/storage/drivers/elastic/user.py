f89b1f2225a3d4d9fbc9212ec3b820dd9910a52a
DawidekZagajnik <krukdawid27@gmail.com>
2022-02-09 23:45:37 +0100

    user site

--- tracardi/service/storage/drivers/elastic/user.py
+++ tracardi/service/storage/drivers/elastic/user.py
@@ -5,7 +5,16 @@ from typing import List, Union
 from tracardi.domain.user import User
 
 
-async def add_user(id: UUID, username: str, password: str, full_name: str, email: str, roles: List[str], disabled: bool):
+async def refresh():
+    return await storage_manager("user").refresh()
+
+
+async def flush():
+    return await storage_manager("user").flush()
+
+
+async def add_user(id: UUID, username: str, password: str, full_name: str, email: str, roles: List[str],
+                   disabled: bool):
     return await storage_manager("user").upsert({
         "id": str(id),
         "username": SHA1Encoder.encode(username),
@@ -17,8 +26,42 @@ async def add_user(id: UUID, username: str, password: str, full_name: str, email
     })
 
 
+async def search_by_name(start: int, limit: int, name: str):
+    query = {
+        "from": start,
+        "size": limit,
+        "query": {
+            "wildcard": {
+                "full_name": f"{name}*"
+            }
+        }
+    }
+    return await storage_manager("user").query(query=query)
+
+
+async def edit_user(id: UUID, username: str, password: str, full_name: str, email: str, roles: List[str],
+                    disabled: bool, password_change: bool):
+    return await storage_manager("user").upsert({
+        "id": str(id),
+        "username": username,
+        "password": SHA1Encoder.encode(password) if password_change else password,
+        "full_name": full_name,
+        "email": email,
+        "roles": roles,
+        "disabled": disabled
+    })
+
+
 async def del_user(id: UUID):
     return await storage_manager("user").delete(str(id))
+
+
+async def get_by_id(id: UUID):
+    return await storage_manager("user").load(str(id))
+
+
+async def load(start: int, limit: int):
+    return await storage_manager("user").load_all(start, limit)
 
 
 async def get_by_login_data(username: str, password: str) -> Union[User, None]:
@@ -27,7 +70,8 @@ async def get_by_login_data(username: str, password: str) -> Union[User, None]:
             "bool": {
                 "must": [
                     {"term": {"username": SHA1Encoder.encode(username)}},
-                    {"term": {"password": SHA1Encoder.encode(password)}}
+                    {"term": {"password": SHA1Encoder.encode(password)}},
+                    {"term": {"disabled": False}}
                 ]
             }
         }
