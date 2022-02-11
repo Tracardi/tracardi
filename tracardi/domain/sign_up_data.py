7d9c8e57bcd1ccd8d60c841e452645211e00f93d
risto <risto.kowaczewski@gmail.com>
2022-02-11 11:17:08 +0100

    Persistence of sign-in

--- tracardi/domain/sign_up_data.py
+++ tracardi/domain/sign_up_data.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel
 
 
+class SignUpRecord(BaseModel):
+    id: str = None
+    host: str = None
+    token: str = None
+
+
 class SignUpData(BaseModel):
     id: str = None
+    host: str
     username: str
     password: str
     type: str
     name: str
-    url: str
-    token: str = None
--- tracardi/service/storage/drivers/elastic/pro.py
+++ tracardi/service/storage/drivers/elastic/pro.py
@@ -1,16 +1,17 @@
 from typing import Optional
 
 from tracardi.domain.entity import Entity
-from tracardi.domain.sign_up_data import SignUpData
+from tracardi.domain.sign_up_data import SignUpRecord
 from tracardi.service.storage.factory import storage_manager, StorageFor
 
 
-async def read_pro_service_endpoint() -> Optional[SignUpData]:
-    return await StorageFor(Entity(id="0")).index("tracardi-pro").load(SignUpData)
+async def read_pro_service_endpoint() -> Optional[SignUpRecord]:
+    return await StorageFor(Entity(id="0")).index("tracardi-pro").load(SignUpRecord)
 
 
-async def save_pro_service_endpoint(sign_up_data: SignUpData):
-    return await storage_manager("tracardi-pro").upsert(sign_up_data.dict())
+async def save_pro_service_endpoint(sign_up_record: SignUpRecord):
+    sign_up_record.id = '0'
+    return await storage_manager("tracardi-pro").upsert(sign_up_record.dict())
 
 
 async def delete_pro_service_endpoint():
