c99b784bdf0bee6922d6c234eb914673c549aae9
DawidekZagajnik <krukdawid27@gmail.com>
2022-03-16 16:41:44 +0100

    query string

--- tracardi/service/storage/drivers/elastic/user_log.py
+++ tracardi/service/storage/drivers/elastic/user_log.py
@@ -2,25 +2,17 @@ from tracardi.service.storage.factory import storage_manager
 from datetime import datetime
 
 
-async def load_logs(start: int = 0, limit: int = 100, email: str = None, lower: int = None, upper: int = None):
+async def load_logs(start: int = 0, limit: int = 100, query_string: str = None):
     query = {
         "query": {
-            "wildcard": {
-                "email": f"{email if email is not None else ''}*"
+            "query_string": {
+                "query": query_string
             }
-        },
+        } if query_string is not None else {"match_all": {}},
         "from": start,
         "size": limit,
         "sort": [
             {"timestamp": "desc"}
         ]
     }
-
-    if lower is not None and upper is not None:
-        query["query"]["range"] = {
-            "timestamp": {
-                "gte": datetime.fromtimestamp(lower),
-                "lte": datetime.fromtimestamp(upper)
-            }
-        }
     return await storage_manager("user-logs").query(query)
