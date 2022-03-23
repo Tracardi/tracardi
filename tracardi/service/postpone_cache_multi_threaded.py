4fc14664f10db0fc75f530b37f44f4e4e92c4de4
risto <risto.kowaczewski@gmail.com>
2022-03-24 00:32:34 +0100

    destinations multi threaded env.

--- tracardi/service/postpone_cache_multi_threaded.py
+++ tracardi/service/postpone_cache_multi_threaded.py
@@ -23,18 +23,45 @@ class PostponeCache:
     def get(self, profile_id) -> bool:
 
         if not self.exists(profile_id):
-            logger.info(f"Create {self.hash}.{profile_id}")
             self.redis.client.hset(self.hash, profile_id, '1')
             return False
-
-        logger.info(f"Exists {self.hash}.{profile_id}")
 
         return True
 
     def set(self, profile_id):
-        logger.info(f"Set {self.hash}.{profile_id}")
         self.redis.client.hset(self.hash, profile_id, '1')
 
     def reset(self, profile_id):
-        logger.debug(f"Clean {self.hash}.{profile_id}")
+        self.redis.client.hdel(self.hash, profile_id)
+
+
+class InstanceCache:
+
+    def __init__(self, cache_type):
+        logger.info(f"Cache for {cache_type} created")
+        self.redis = RedisClient(redis_config.redis_host)
+        self.hash = cache_type
+
+    def exists(self, profile_id):
+        return self.redis.client.hexists(self.hash, profile_id)
+
+    def get_instance(self, profile_id, instance_id) -> Optional[str]:
+
+        if not self.exists(profile_id):
+            logger.info(f"Create instance {self.hash}.{profile_id} = {instance_id}")
+            self.redis.client.hset(self.hash, profile_id, instance_id)
+            return None
+
+        value_bson = self.redis.client.hget(self.hash, profile_id)
+        value = value_bson.decode('utf-8')
+        logger.info(f"Got instance {value} from {self.hash}.{profile_id}")
+
+        return value
+
+    def set_instance(self, profile_id, instance_id):
+        logger.info(f"Set instance {instance_id} for profile {profile_id}")
+        self.redis.client.hset(self.hash, profile_id, instance_id)
+
+    def reset(self, profile_id):
+        logger.debug(f"Clean instance {self.hash}.{profile_id}")
         self.redis.client.hdel(self.hash, profile_id)
--- tracardi/service/postpone_call.py
+++ tracardi/service/postpone_call.py
@@ -3,15 +3,15 @@ import logging
 
 from tracardi.config import tracardi
 from tracardi.exceptions.log_handler import log_handler
-from tracardi.service.postpone_cache_multi_threaded import PostponeCache
+from tracardi.service.postpone_cache_multi_threaded import PostponeCache, InstanceCache
 
 logger = logging.getLogger(__name__)
 logger.setLevel(tracardi.logging_level)
 logger.addHandler(log_handler)
 
-schedule_flag_cache = PostponeCache("schedule-flag")
-postpone_flag_cache = PostponeCache("postpone-flag")
-instance_cache = PostponeCache("exec-instance-cache")
+global_schedule_flag = PostponeCache("schedule-flag")
+global_postpone_flag = PostponeCache("postpone-flag")
+instance_cache = InstanceCache("exec-instance-cache")
 
 scheduled_locally = False
 
@@ -29,10 +29,10 @@ class PostponedCall:
 
         # We must keep info about the loop.call_later being called in scheduled_locally variable, as if the instance
         # dies (loop.call_later is cancelled) and there is a global flag that loop.call_later is running but locally it
-        # is not. Sho we must recreate it.
+        # is not. So we must recreate it.
 
         global scheduled_locally
-        loop.call_later(self.wait, self._postpone_call, loop)
+        loop.call_later(self.wait, self._execute, loop)
         scheduled_locally = True
 
     def _run_scheduled(self):
@@ -40,31 +40,31 @@ class PostponedCall:
         asyncio.ensure_future(self.callable_coroutine(*self.args))
         scheduled_locally = False
 
-    def _postpone_call(self, loop):
+    def _execute(self, loop):
+
+        global_instance = instance_cache.get_instance(self.profile_id, None)
+        if global_instance != self.instance_id:
+            logger.info(
+                f"Execution on instance {self.instance_id} discarded. Execution passed to {global_instance}")
+
         try:
-            global_postpone_flag = postpone_flag_cache.get(self.profile_id)
-
             # should the execution be postponed. If there was no second call then postpone is None.
-            if global_postpone_flag is None:
+            if global_postpone_flag.get(self.profile_id) is False:
                 # it is not postponed - run it now
-
-                global_instance = instance_cache.get(self.profile_id)
-                if global_instance == self.instance_id:
-                    # run it only if the local instance is the same as global schedule instance
-                    self._run_scheduled()
-                else:
-                    logger.info(f"Execution on instance {self.instance_id} discarded. Execution passed to {global_instance}")
+                self._run_scheduled()
 
                 # clean cache
-                postpone_flag_cache.reset(self.profile_id)
-                schedule_flag_cache.reset(self.profile_id)
+                global_postpone_flag.reset(self.profile_id)
+                global_schedule_flag.reset(self.profile_id)
+                instance_cache.reset(self.profile_id)
+
             else:
                 # postpone call. Postpone flag is true
                 self._schedule_for_later(loop)
-                logger.info(f"Execution postponed for profile {self.profile_id}. Execute on instance {self.instance_id}")
+                logger.info(
+                    f"Execution postponed for profile {self.profile_id}. Execute on instance {self.instance_id}")
                 # delete postpone flag. It can be set again if there is another call.
-                postpone_flag_cache.reset(self.profile_id)
-                schedule_flag_cache.set(self.profile_id)
+                global_postpone_flag.reset(self.profile_id)
 
         except Exception as e:
             logger.error(str(e))
@@ -73,16 +73,13 @@ class PostponedCall:
         # set current instance
         instance_cache.set_instance(self.profile_id, self.instance_id)
 
-        # set global schedule flag
-        global_schedule_flag = schedule_flag_cache.get_instance(self.profile_id, self.instance_id)
-
         if not scheduled_locally:
             # if there is no schedule local. Schedule for the first time.
             self._schedule_for_later(loop)
 
-        if global_schedule_flag is None:
+        if global_schedule_flag.get(self.profile_id) is False:
             # mark as scheduled globally
-            schedule_flag_cache.set_instance(self.profile_id, self.instance_id)
+            global_schedule_flag.set(self.profile_id)
         else:
             # if this is a second call then postpone
-            postpone_flag_cache.set_instance(self.profile_id, self.instance_id)
+            global_postpone_flag.set(self.profile_id)
