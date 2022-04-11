db8811c5034282bd47e259f124933251a6eb2607
risto <risto.kowaczewski@gmail.com>
2022-04-11 21:53:25 +0200

    redis password

--- tracardi/service/storage/redis_client.py
+++ tracardi/service/storage/redis_client.py
@@ -1,6 +1,7 @@
 import aioredis
 import redis
 from tracardi.service.singleton import Singleton
+from tracardi.config import redis_config
 
 
 class AsyncRedisClient(metaclass=Singleton):
@@ -9,5 +10,11 @@ class AsyncRedisClient(metaclass=Singleton):
 
 
 class RedisClient(metaclass=Singleton):
-    def __init__(self, host):
-        self.client = redis.from_url(host)
+    def __init__(self):
+        host = redis_config.redis_host
+        password = redis_config.redis_password
+
+        if password is None:
+            self.client = redis.from_url(host)
+        else:
+            self.client = redis.from_url(host, password=password)
