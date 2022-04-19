642183c60604bbba521aaceb99fedbbff7dfb159
DawidekZagajnik <60825727+DawidekZagajnik@users.noreply.github.com>
2022-04-19 20:32:33 +0200

    dead instance removal (#427)

--- tracardi/service/storage/drivers/elastic/api_instance.py
+++ tracardi/service/storage/drivers/elastic/api_instance.py
@@ -3,3 +3,13 @@ from tracardi.service.storage.factory import storage_manager
 
 async def load_all(start: int = 0, limit: int = 100):
     return await storage_manager('api-instance').load_all(start, limit)
+
+
+async def remove_dead_instances():
+    return await storage_manager('api-instance').delete_by_query({
+        "query": {
+            "query_string": {
+                "query": "timestamp:[* TO now-1d]"
+            }
+        }
+    })
