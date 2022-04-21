3091443cf8bea9afdf0330f8f06987bdf1df7bd4
risto <risto.kowaczewski@gmail.com>
2022-04-21 19:18:02 +0200

    TPRO scheduler plugin.

--- tracardi/process_engine/action/v1/pro/scheduler/plugin.py
+++ tracardi/process_engine/action/v1/pro/scheduler/plugin.py
@@ -1,15 +1,22 @@
 import json
+import logging
 from datetime import datetime, timedelta
 
 import tracardi.config
 from tracardi.domain.scheduler_config import SchedulerConfig
 from tracardi.domain.resource import ResourceCredentials
+from tracardi.exceptions.log_handler import log_handler
 from tracardi.process_engine.action.v1.pro.scheduler.model.configuration import Configuration
 from tracardi.process_engine.action.v1.pro.scheduler.service.schedule_client import SchedulerClient
 from tracardi.service.storage.driver import storage
 from tracardi.service.plugin.runner import ActionRunner
 from tracardi.service.plugin.domain.register import Plugin, Spec, MetaData, Form, FormGroup, FormField, FormComponent
 from tracardi.service.plugin.domain.result import Result
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+logger.setLevel(tracardi.config.tracardi.logging_level)
+logger.addHandler(log_handler)
 
 
 def validate(config: dict) -> Configuration:
@@ -32,26 +39,43 @@ class SchedulerPlugin(ActionRunner):
             output=SchedulerConfig)  # type: SchedulerConfig
 
     async def run(self, payload):
+        try:
+            client = SchedulerClient(tracardi.config.tracardi.tracardi_pro_host)
+            schedule_at = str(datetime.utcnow() + timedelta(seconds=self.config.postpone))
+            result = await client.schedule(
+                schedule_at=schedule_at,
+                callback_host=self.credentials.callback_host,
+                source_id=self.event.source.id,
+                profile_id=self.profile.id,
+                session_id=self.session.id,
+                event_type=self.config.event_type,
+                properties=json.loads(self.config.properties),
+                context=self.event.context
+            )
 
-        run_in_background = True
+            response = {
+                "response": {
+                    "id": result.id,
+                    "time": result.time,
+                    "key": result.key,
+                    "origin": result.origin,
+                    "server": tracardi.config.tracardi.tracardi_pro_host
+                },
+                "payload": payload
+            }
 
-        client = SchedulerClient(tracardi.config.tracardi.tracardi_pro_host)
-        schedule_at = str(datetime.utcnow() + timedelta(seconds=self.config.postpone))
-        result = await client.schedule(
-            schedule_at=schedule_at,
-            callback_host=self.credentials.callback_host,
-            source_id=self.event.source.id,
-            profile_id=self.profile.id,
-            session_id=self.session.id,
-            event_type=self.config.event_type,
-            properties=json.loads(self.config.properties),
-            context=self.event.context
-        )
-        print(f"XXX {result.id}")
-        if not run_in_background:
-            return Result(port="response", value=None)
-        else:
-            return Result(port="response", value=None)
+            message = f"Scheduled task id: {result.id}"
+            self.console.log(message)
+            logger.info(message)
+
+            return Result(port="response", value=response)
+        except Exception as e:
+            logger.error(str(e))
+            self.console.error(str(e))
+            return Result(port="error", value={
+                "message": str(e),
+                "payload": payload
+            })
 
 
 def register() -> Plugin:
