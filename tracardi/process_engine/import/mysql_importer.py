26091d5a7974e2cfcb4f60c52dda607230880873
risto <risto.kowaczewski@gmail.com>
2022-05-10 15:50:59 +0200

    Data importer

--- tracardi/process_engine/import/mysql_importer.py
+++ tracardi/process_engine/import/mysql_importer.py
@@ -124,6 +124,6 @@ class MySQLImporter(Importer):
         config = MySQLImportConfig(**config)
         resource = await storage.driver.resource.load(config.source.id)
         credentials = resource.credentials.test if self.debug is True else resource.credentials.production
-        config = {**config, **credentials}
-
+        config = {**config.dict(), **credentials}
+        print(config)
         # TODO ADD TO CELERY WITH config VARIABLE PASSED TO FUNC
