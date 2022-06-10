17fbb58305339c03f10cec1fb1f45274eff91a9d
DawidekZagajnik <krukdawid27@gmail.com>
2022-06-10 17:55:56 +0200

    migration setup, needs testing

---
+++ tracardi/domain/migration_schema.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class MigrationSchema(BaseModel):
+    id: str
+    name: str
+    multi: bool
+    script: Optional[str] = None
+    worker: str
+    asynchronous: bool
+    from_index: Optional[str] = None
+    to_index: Optional[str] = None
