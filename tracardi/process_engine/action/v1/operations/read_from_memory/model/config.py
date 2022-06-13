cf0bb008558445df8a2a000595d028fcb7e0f7b4
risto <risto.kowaczewski@gmail.com>
2022-06-13 21:36:24 +0200

    #505 fix

--- tracardi/process_engine/action/v1/operations/read_from_memory/model/config.py
+++ tracardi/process_engine/action/v1/operations/read_from_memory/model/config.py
@@ -1,5 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Config(BaseModel):
     key: str
+
+    @validator("key")
+    def must_not_be_empty(cls, value):
+        if value == "":
+            raise ValueError("This field can not be empty")
+        return value
