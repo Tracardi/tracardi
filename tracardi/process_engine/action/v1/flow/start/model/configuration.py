1727d7fc666a40955479fdb94587a4352a9a24c4
DawidekZagajnik <krukdawid27@gmail.com>
2022-06-17 17:50:25 +0200

    start and debug node merge

--- tracardi/process_engine/action/v1/flow/start/model/configuration.py
+++ tracardi/process_engine/action/v1/flow/start/model/configuration.py
@@ -1,12 +1,25 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, validator
+import json
 
 from tracardi.domain.named_entity import NamedEntity
 
 
 class Configuration(BaseModel):
     debug: bool = False
-    events: List[NamedEntity] = []
+    event_types: List[NamedEntity] = []
+
+    profile_less: Optional[bool] = False
+    session_less: Optional[bool] = False
+    properties: Optional[str] = "{}"
+    event_id: Optional[str] = None
+    event_type: Optional[NamedEntity] = None
+
+    @validator("properties")
+    def convert_to_json_id_dict(cls, value):
+        if isinstance(value, dict):
+            value = json.dumps(value)
+        return value
 
     def get_allowed_event_types(self) -> List[str]:
-        return [event.id for event in self.events]
+        return [event.id for event in self.event_types]
