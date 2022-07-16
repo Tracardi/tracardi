38532cf2bf82e4fa69bfd642631442f30f1df668
Dawid Kruk <krukdawid27@gmail.com>
2022-07-16 23:03:57 +0200

    user account expiration date (#554)

--- tracardi/domain/user.py
+++ tracardi/domain/user.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 from tracardi.service.sha1_hasher import SHA1Encoder
+from datetime import datetime
 
 
 class User(BaseModel):
@@ -12,12 +13,16 @@ class User(BaseModel):
     roles: List[str]
     disabled: bool = False
     token: Optional[str] = None
+    expiration_timestamp: Optional[int] = None
 
     def encode_password(self):
         self.password = SHA1Encoder.encode(self.password)
 
     def has_roles(self, roles) -> bool:
         return len(set(self.roles).intersection(set(roles))) > 0
+
+    def is_expired(self) -> bool:
+        return False if self.expiration_timestamp is None else self.expiration_timestamp <= datetime.utcnow().timestamp()
 
     def is_the_same_user(self, id):
         return self.id == id
