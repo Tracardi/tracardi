e3f9deb24fe718716d5eb528f9741f3bc6c9d6f2
DawidekZagajnik <krukdawid27@gmail.com>
2022-07-30 21:22:49 +0200

    event aggregator field

--- tracardi/process_engine/action/v1/events/event_aggregator/model/configuration.py
+++ tracardi/process_engine/action/v1/events/event_aggregator/model/configuration.py
@@ -1,15 +1,16 @@
 from pydantic import validator
 from pytimeparse import parse
 from tracardi.service.plugin.domain.config import PluginConfig
+from tracardi.domain.named_entity import NamedEntity
 
 
 class Configuration(PluginConfig):
-    field: str
+    field: NamedEntity
     time_span: str
 
     @validator("field")
     def field_not_empty(cls, value):
-        if len(value) == 0:
+        if not value or (isinstance(value, NamedEntity) and not value.id):
             raise ValueError("Field name can not be empty")
         return value
 
