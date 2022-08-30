dcf7599a2daad3fe6e4cd91f03ffb11145a025ab
Dawid Kruk <krukdawid27@gmail.com>
2022-08-30 10:49:02 +0200

    expression transformer refactoring, CONTAINS, STARTS WITH, ENDS WITH operators implementation (#669)

--- tracardi/process_engine/tql/transformer/expr_transformer.py
+++ tracardi/process_engine/tql/transformer/expr_transformer.py
@@ -8,11 +8,12 @@ import pytz
 from tracardi.config import tracardi
 from tracardi.exceptions.log_handler import log_handler
 from tracardi.service.notation.dot_accessor import DotAccessor
+from typing import Union
 
 from ..domain.field import Field
 from .function_transformer import FunctionTransformer
 from .transformer_namespace import TransformerNamespace
-from ..domain.missing_value import MissingValue
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(tracardi.logging_level)
@@ -42,6 +43,65 @@ operation_mapper = {
 
 
 class ExprTransformer(TransformerNamespace):
+
+    __mapping = {
+        ("now", 0): datetime.datetime.now,
+        ("utcnow", 0): datetime.datetime.utcnow,
+        ("datetime", 1): "_datetime1",
+        ("now", 1): "_now1",
+        ("now.offset", 1): "_now_offset1",
+        ("now.timezone.offset", 2): "_now_timezone_offset2",
+        ("datetime.offset", 2): "_datetime_offset2",
+        ("datetime.timezone", 2): "_datetime_timezone2",
+        ("datetime.from_timestamp", 1): "_datetime_from_timestamp1",
+        ("now.timezone", 1): "_now_timezone1",
+        ("lowercase", 1): "_lowercase1",
+        ("uppercase", 1): "_uppercase1"
+    }
+
+    def _datetime1(self, value):
+        return self.__parse_datetime(value)
+
+    def _now1(self, timezone):
+        return datetime.datetime.now(pytz.timezone(timezone))
+
+    def _now_offset1(self, offset):
+        passed_seconds = self.__parse_offset(offset)
+        return datetime.datetime.now() + datetime.timedelta(seconds=passed_seconds)
+
+    def _now_timezone_offset2(self, timezone, offset):
+        passed_seconds = self.__parse_offset(offset)
+        timezone = pytz.timezone(timezone)
+        return datetime.datetime.now(timezone) + datetime.timedelta(seconds=passed_seconds)
+
+    def _datetime_offset2(self, date, offset):
+        passed_seconds = self.__parse_offset(offset)
+        date = self.__parse_datetime(date)
+        return date + datetime.timedelta(seconds=passed_seconds)
+
+    def _datetime_timezone2(self, date, timezone):
+        date = self.__parse_datetime(date)
+        timezone = pytz.timezone(timezone)
+        tz_date = date.replace(tzinfo=pytz.utc).astimezone(timezone)
+        return timezone.normalize(tz_date)
+
+    def _datetime_from_timestamp1(self, timestamp):
+        return datetime.datetime.fromtimestamp(timestamp)
+
+    def _now_timezone1(self, timezone):
+        timezone = pytz.timezone(timezone)
+        tz_date = datetime.datetime.now().replace(tzinfo=pytz.utc).astimezone(timezone)
+        return timezone.normalize(tz_date)
+
+    def _lowercase1(self, value):
+        if isinstance(value, str):
+            return value.lower()
+        return value
+
+    def _uppercase1(self, value):
+        if isinstance(value, str):
+            return value.upper()
+        return value
 
     def __init__(self, dot, *args, **kwargs):
         if not isinstance(dot, DotAccessor):
@@ -146,96 +206,12 @@ class ExprTransformer(TransformerNamespace):
         else:
             values = []
 
-        if not values or values[0] is None:
-            if function == 'now':
-                return datetime.datetime.now()
-            if function == 'utcnow':
-                return datetime.datetime.utcnow()
-        else:
-            if function == 'datetime' and len(values) == 1:
-                value = values[0]
+        method = self.__mapping.get((function, len(values)), None)
+        if method is None:
+            raise ValueError("Unknown operation `{}`".format(function))
 
-                if isinstance(value, str):
-                    date = dateparser.parse(value)
-
-                    if not date:
-                        raise ValueError("Could not parse date `{}`".format(value))
-                elif isinstance(value, datetime.datetime):
-                    return value
-                else:
-                    raise ValueError(
-                        "Value of `{}` must be string to compare it with datetime. Type of {} given".format(value,
-                                                                                                            type(
-                                                                                                                value)))
-                return date
-
-            if function == 'now' and len(values) == 1:
-                timezone = values[0]
-                return datetime.datetime.now(pytz.timezone(timezone))
-
-            if function == 'now.offset' and len(values) == 1:
-                offset = values[0]
-                passed_seconds = pytimeparse.parse(offset)
-                if passed_seconds is None:
-                    raise ValueError("Could not parse `{}`".format(offset))
-                return datetime.datetime.now() + datetime.timedelta(seconds=passed_seconds)
-
-            if function == 'now.timezone.offset' and len(values) == 2:
-                timezone, offset = values
-                passed_seconds = pytimeparse.parse(offset)
-                if passed_seconds is None:
-                    raise ValueError("Could not parse `{}`".format(offset))
-                timezone = pytz.timezone(timezone)
-                return datetime.datetime.now(timezone) + datetime.timedelta(seconds=passed_seconds)
-
-            if function == 'datetime.offset' and len(values) == 2:
-                date, offset = values
-
-                if isinstance(date, MissingValue):
-                    return MissingValue
-
-                passed_seconds = pytimeparse.parse(offset)
-                if passed_seconds is None:
-                    raise ValueError("Could not parse `{}`".format(offset))
-
-                if isinstance(date, str):
-                    date = dateparser.parse(date)
-
-                return date + datetime.timedelta(seconds=passed_seconds)
-
-            if function == 'datetime.timezone' and len(values) == 2:
-                date, timezone = values
-
-                if isinstance(date, str):
-                    date = dateparser.parse(date)
-
-                timezone = pytz.timezone(timezone)
-                tz_date = date.replace(tzinfo=pytz.utc).astimezone(timezone)
-                return timezone.normalize(tz_date)
-
-            if function == 'datetime.from_timestamp' and len(values) == 1:
-                timestamp, = values
-                return datetime.datetime.fromtimestamp(timestamp)
-
-            if function == 'now.timezone' and len(values) == 1:
-                timezone, = values
-                timezone = pytz.timezone(timezone)
-                tz_date = datetime.datetime.now().replace(tzinfo=pytz.utc).astimezone(timezone)
-                return timezone.normalize(tz_date)
-
-            if function == 'lowercase' and len(values) == 1:
-                value = values[0]
-                if isinstance(value, str):
-                    return value.lower()
-                return value
-
-            if function == 'uppercase' and len(values) == 1:
-                value = values[0]
-                if isinstance(value, str):
-                    return value.upper()
-                return value
-
-        raise ValueError("Unknown type `{}`".format(function))
+        func = getattr(self, method) if isinstance(method, str) else method
+        return func(*values)
 
     def op_field_sig(self, args):
         return args[0]
@@ -270,3 +246,76 @@ class ExprTransformer(TransformerNamespace):
             return not self.op_empty(args)
         except AttributeError:
             return True
+
+    def op_contains(self, args):
+        container, _, contained = args
+        if isinstance(container.value, str) and not isinstance(contained, str):
+            raise ValueError("Value tested for containing value is of type string, but value tested for being "
+                             "contained is not. {} given.".format(type(contained)))
+        if not (isinstance(container.value, str) or isinstance(container.value, list)):
+            raise ValueError(
+                "Value tested for containing other value, should be array or string. {} given.".format(
+                    type(container.value)
+                )
+            )
+
+        return contained in container.value
+
+    def op_startswith(self, args):
+        container, _, prefix = args
+        if isinstance(container.value, list):
+            if not container.value:
+                return False
+            return container.value[0] == prefix
+
+        elif isinstance(container.value, str):
+            return container.value.startswith(prefix)
+
+        else:
+            raise ValueError(
+                "Value tested for starting with other value, should be array or string. {} given.".format(
+                    type(container.value)
+                )
+            )
+
+    def op_endswith(self, args):
+        container, _, suffix = args
+        if isinstance(container.value, list):
+            if not container.value:
+                return False
+            return container.value[-1] == suffix
+
+        elif isinstance(container.value, str):
+            return container.value.endswith(suffix)
+
+        else:
+            raise ValueError(
+                "Value tested for ending with other value, should be array or string. {} given.".format(
+                    type(container.value)
+                )
+            )
+
+
+    @staticmethod
+    def __parse_datetime(value: Union[str, datetime.datetime]) -> datetime.datetime:
+        if isinstance(value, datetime.datetime):
+            return value
+        elif isinstance(value, str):
+            value = dateparser.parse(value)
+            if not value:
+                raise ValueError("Could not parse `{}` as date.. Expected datetime or parsable string.".format(value))
+            return value
+        raise ValueError("Could not parse `{}` as date. Expected datetime or parsable string.".format(value))
+
+    @staticmethod
+    def __parse_offset(value: str) -> Union[int, float]:
+        try:
+            value = pytimeparse.parse(value)
+            if not value:
+                raise ValueError("Could not parse value `{}` as time offset.".format(value))
+            return value
+        except Exception as _:
+            raise ValueError("Could not parse value `{}` as time offset.".format(value))
+
+
+
