3002027abd8215c6b5fb8c713aeaa8fed30974a1
yk-jp <k82.y12@gmail.com>
2022-10-11 12:14:38 -0700

    add a new validator for a delimiter

--- tracardi/process_engine/action/v1/strings/string_join/model/configuration.py
+++ tracardi/process_engine/action/v1/strings/string_join/model/configuration.py
@@ -9,5 +9,11 @@ class Configuration(PluginConfig):
     @validator('string')
     def string_must_not_be_empty(cls, value):
         if len(value.strip()) <= 0:
-            raise ValueError('String must not be an empty')
+            raise ValueError('String must not be empty')
+        return value
+
+    @validator('delimiter')
+    def delimiter_must_be_single_character(cls, value):
+        if len(value) != 1:
+            raise ValueError('Delimiter must be a single character')
         return value
