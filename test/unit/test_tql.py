6326bc0d95eef29dc21eb074f337d5ab1a25a258
risto <risto.kowaczewski@gmail.com>
2022-11-27 18:35:38 +0100

    Fixed compare error.

--- test/unit/test_tql.py
+++ test/unit/test_tql.py
@@ -50,6 +50,10 @@ def test_tql_between():
 
 
 def test_tql_equal():
+
+    tree = parser.parse("payload@a.b == payload@a.c")
+    assert ExprTransformer(dot=dot).transform(tree)
+
     tree = parser.parse("payload@a.e == \"test\"")
     assert ExprTransformer(dot=dot).transform(tree)
 
