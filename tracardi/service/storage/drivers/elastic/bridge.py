77c694b568b58ee980a96da613d55e869711d46a
risto <risto.kowaczewski@gmail.com>
2022-12-06 11:17:55 +0100

    Bridge registering.

--- tracardi/service/storage/drivers/elastic/bridge.py
+++ tracardi/service/storage/drivers/elastic/bridge.py
@@ -1,8 +1,14 @@
+import logging
 from typing import Optional
 
+from tracardi.config import tracardi
 from tracardi.domain.bridge import Bridge
 from tracardi.domain.storage_record import StorageRecords, StorageRecord
 from tracardi.service.storage.factory import storage_manager
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(tracardi.logging_level)
 
 
 async def load_all() -> StorageRecords:
@@ -20,3 +26,10 @@ async def delete_by_id(id: str) -> dict:
 
 async def save(bridge: Bridge):
     return await storage_manager('bridge').upsert(bridge)
+
+
+async def install_bridge(bridge: Bridge):
+    source = await load_by_id(bridge.id)
+    if source is None:
+        result = await save(bridge)
+        logger.info(f"Bridge registered with id {bridge.id} and result {result}")
