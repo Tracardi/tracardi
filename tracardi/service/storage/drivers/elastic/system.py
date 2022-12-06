24ef61395b44b6049ed9eb5c2361f3d9eeb68052
risto <risto.kowaczewski@gmail.com>
2022-12-06 10:37:11 +0100

    docker

--- tracardi/service/storage/drivers/elastic/system.py
+++ tracardi/service/storage/drivers/elastic/system.py
@@ -3,17 +3,16 @@ from typing import Tuple
 from tracardi.service.storage.indices_manager import get_indices_status
 
 
-async def is_schema_ok() -> Tuple[bool, list]:
+def get_missing(indices, type) -> list:
+    return [idx[1] for idx in indices if idx[0] == type]
 
+
+async def is_schema_ok() -> Tuple[bool, list]:
     # Missing indices
 
     _indices = [item async for item in get_indices_status()]
-    missing_indices = [idx[1] for idx in _indices if idx[0] == 'missing_index']
-    # existing_indices = [idx[1] for idx in _indices if idx[0] == 'existing_index']
-    # missing_templates = [idx[1] for idx in _indices if idx[0] == 'missing_template']
-    missing_aliases = [idx[1] for idx in _indices if idx[0] == 'missing_alias']
-    # existing_aliases = [idx[1] for idx in _indices if idx[0] == 'existing_alias']
-    # existing_templates = [idx[1] for idx in _indices if idx[0] == 'existing_template']
+    missing_indices = get_missing(_indices, type='missing_index')
+    missing_aliases = get_missing(_indices, type='missing_alias')
 
     is_schema_ok = not missing_indices and not missing_aliases and not missing_aliases
 
