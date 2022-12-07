75bbb5a3d29f27a4da289ec30497bd99c3bb84a1
risto <risto.kowaczewski@gmail.com>
2022-12-08 00:48:29 +0100

    saveSession does not make reference to session in event even if session exists.

--- tracardi/service/tracker_persister.py
+++ tracardi/service/tracker_persister.py
@@ -166,10 +166,7 @@ class TrackerResultPersister:
             # Reset session id if session is not saved
 
             if tracker_result.tracker_payload.is_on('saveSession', default=True) is False:
-                # DO NOT remove session if it already exists in db
-                if not isinstance(event.session, Entity) or not await storage.driver.session.exist(
-                        event.session.id):
-                    event.session = None
+                event.session = None
 
             if event.id in log_event_journal:
                 log = log_event_journal[event.id]
