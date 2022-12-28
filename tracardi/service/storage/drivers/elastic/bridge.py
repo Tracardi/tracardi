4552a93324653338b6efa629a6e71471bc6936d1
risto <risto.kowaczewski@gmail.com>
2022-12-28 22:12:39 +0100

    Locks event source when unregistered bridge

--- tracardi/service/storage/drivers/elastic/bridge.py
+++ tracardi/service/storage/drivers/elastic/bridge.py
@@ -4,11 +4,12 @@ from typing import Optional
 from tracardi.config import tracardi
 from tracardi.domain.bridge import Bridge
 from tracardi.domain.storage_record import StorageRecords, StorageRecord
+from tracardi.exceptions.log_handler import log_handler
 from tracardi.service.storage.factory import storage_manager
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 logger.setLevel(tracardi.logging_level)
+logger.addHandler(log_handler)
 
 
 async def load_all() -> StorageRecords:
@@ -29,7 +30,7 @@ async def save(bridge: Bridge):
 
 
 async def install_bridge(bridge: Bridge):
-    source = await load_by_id(bridge.id)
-    if source is None:
+    bridge_record = await load_by_id(bridge.id)
+    if bridge_record is None:
         result = await save(bridge)
         logger.info(f"Bridge registered with id {bridge.id} and result {result}")
--- tracardi/service/storage/drivers/elastic/event_source.py
+++ tracardi/service/storage/drivers/elastic/event_source.py
@@ -1,7 +1,15 @@
+import logging
 from typing import Tuple, List, Optional
+
+from tracardi.config import tracardi
 from tracardi.domain.value_object.bulk_insert_result import BulkInsertResult
 from tracardi.domain.event_source import EventSource
+from tracardi.exceptions.log_handler import log_handler
 from tracardi.service.storage.factory import storage_manager
+
+logger = logging.getLogger(__name__)
+logger.setLevel(tracardi.logging_level)
+logger.addHandler(log_handler)
 
 
 async def refresh():
@@ -31,6 +39,10 @@ async def load_by_tag(tag):
     return await storage_manager('event-source').load_by('tags', tag)
 
 
+async def load_by_bridge(bridge_id):
+    return await storage_manager('event-source').load_by('bridge.id', bridge_id)
+
+
 async def load_by(field, value):
     return await storage_manager('event-source').load_by(field, value)
 
@@ -42,3 +54,14 @@ async def delete_by_id(id: str):
 
 async def save(event_source: EventSource) -> BulkInsertResult:
     return await storage_manager('event-source').upsert(event_source)
+
+
+async def unlock_event_source_by_bridge(bridge_id):
+    event_source_records = await load_by_bridge(bridge_id)
+    for event_source_record in event_source_records:
+        locked = event_source_record.get('locked', False)
+        if locked:
+            event_source = event_source_record.to_entity(EventSource)
+            event_source.locked = False
+            lock_result = await save(event_source)
+            logger.info(f"Event source `{event_source.name}` unlocked with result {lock_result}")
