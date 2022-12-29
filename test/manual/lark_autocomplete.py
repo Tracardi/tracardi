207d750c960e9b7f93e0ce21f435bc63ed97e47e
risto <risto.kowaczewski@gmail.com>
2022-12-30 00:50:33 +0100

    Autocomplete

--- test/manual/lark_autocomplete.py
+++ test/manual/lark_autocomplete.py
@@ -3,17 +3,20 @@ from lark import Lark
 
 schema = r"""
 ?start: multi_expr
-multi_expr: expr (OR_TERMINAL|AND_TERMINAL) expr
-expr:       statement 
-            | or_multiple_joins
-            | and_multiple_joins
-            | multi_expr
-            
+multi_expr: statement 
+            | or_expr
+            | and_expr 
+            | and_expr (OR_TERMINAL (and_expr|or_expr))*
+            | or_expr (AND_TERMINAL (and_expr|or_expr))*
+or_expr: or_multiple_joins
+            | or_join
+and_expr: and_multiple_joins
+            | and_join
 or_multiple_joins: (OPEN_BRACKET ( or_join (OR_TERMINAL statement)*) CLOSE_BRACKET)
 and_multiple_joins: (OPEN_BRACKET ( and_join (AND_TERMINAL statement)*) CLOSE_BRACKET)
 
-or_join:    statement OR_TERMINAL statement
-and_join:   statement AND_TERMINAL statement
+or_join:    (statement|or_multiple_joins|and_multiple_joins) OR_TERMINAL (statement|or_multiple_joins|and_multiple_joins)
+and_join:   (statement|or_multiple_joins|and_multiple_joins) AND_TERMINAL (statement|or_multiple_joins|and_multiple_joins)
 statement: FIELD OP (QUOTE VALUE QUOTE | VALUE)
 OPEN_BRACKET: "("
 CLOSE_BRACKET: ")"
@@ -36,11 +39,10 @@ OR_TERMINAL: /OR/i
 # OR_TERMINAL: /(\r? \n|\s)+OR(\r? \n|\s)+/i
 # or_multiple_joins: (OPEN_BRACKET ( or_join | or_join OR_TERMINAL or_join) CLOSE_BRACKET)
 
-parser = Lark(schema, parser="lalr")
+parser = Lark(schema, parser="lalr", debug = True)
 
 interactive = parser.parse_interactive("""
-a:as or (as:a and sas:as) and (sss>1 or d:a or d:a or d:a or d:a ) and sd:as or sad:ass or sdsada:as or ssd:asas
-
+(sa:as or ((sd:asa or sds:as)and ddd:as))
 """)
 # (s:"d" or d:s or (as:a and d:ss))
 # feeds the text given to above into the parsers. This is not done automatically.
