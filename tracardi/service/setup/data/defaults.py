4420eab67fc8c83bd3d6c72316a856cba7836576
risto <risto.kowaczewski@gmail.com>
2023-01-06 22:12:02 +0100

    Fix error of duplicated Bridge.

--- tracardi/service/setup/data/defaults.py
+++ tracardi/service/setup/data/defaults.py
@@ -1,11 +1,9 @@
-from uuid import uuid4
-
 from tracardi.domain.bridge import Bridge
 
 default_db_data = {
     "bridge": [
         Bridge(
-            id=str(uuid4()),
+            id="778ded05-4ff3-4e08-9a86-72c0195fa95d",
             type="rest",
             name="API Bridge Open-source",
             description="Opens and API and collects from it"
