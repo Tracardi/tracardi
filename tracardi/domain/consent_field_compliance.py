a52172d3140543f67d6b02c6494dbeb7aad64d0d
risto <risto.kowaczewski@gmail.com>
2023-01-09 11:48:25 +0100

    Data compliance

--- tracardi/domain/consent_field_compliance.py
+++ tracardi/domain/consent_field_compliance.py
@@ -1,18 +1,20 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 from tracardi.domain.entity import Entity
 from tracardi.domain.named_entity import NamedEntity
+from tracardi.domain.ref_value import RefValue
 
 
 class ConsentFieldComplianceSetting(BaseModel):
-    field: str
-    consent_id: str
     action: str  # Remove, Hash, Do nothing
+    field: RefValue
+    consents: List[NamedEntity]
 
 
 class ConsentFieldCompliance(Entity):
     name: str
+    description: Optional[str] = ""
     event_type: NamedEntity
-    settings: List[ConsentFieldComplianceSetting]
+    settings: List[ConsentFieldComplianceSetting]  # Flattened ES field
