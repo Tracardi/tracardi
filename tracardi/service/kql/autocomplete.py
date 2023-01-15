2d511fb8063baea22bdfcac28d950cf36b4f6f4f
risto <risto.kowaczewski@gmail.com>
2023-01-15 13:02:39 +0100

    Autocomplete

--- tracardi/service/kql/autocomplete.py
+++ tracardi/service/kql/autocomplete.py
@@ -1,4 +1,6 @@
-from typing import List
+from dataclasses import dataclass
+from hashlib import sha1
+from typing import List, Optional, Generator, Tuple, Dict
 
 from lark import Lark, Token
 from lark.lexer import TerminalDef
@@ -25,6 +27,7 @@ QUOTE:      /[\"]/
 VALUE:      /([^\s\"]+|(?<!\\)([\"].*?(?<!\\)[\"]))/ 
             | "*"
 FIELD:      /[a-zA-z_\.-0-9]+/
+SPACE:      /[\s]+/
 operator:   ":" 
             | ">=" 
             | "<=" 
@@ -34,42 +37,64 @@ OP: /(:|<=|>=|>|<)/
 AND: /AND/i
 OR: /OR/i
 
-%import common.ESCAPED_STRING
 %ignore /\s+/
 """
+
+
 # (\w+|(?<!\\)([\"].*?(?<!\\)[\"]))
+# %import common.ESCAPED_STRING
+
+@dataclass
+class Value:
+    token: str
+    value: Optional[str] = None
+
+
+@dataclass
+class AutocompleteValue:
+    next_token: Value
+    current_token: Value
+
+
+class HashableDict(Dict):
+    def __hash__(self):
+        return sha1(self['value'].encode())
+
 
 class Values:
 
     @staticmethod
-    async def _quote(field):
+    async def _quote(field, current_value):
         return ['"']
 
     @staticmethod
-    async def _op(field):
+    async def _op(field, current_value):
         return [':', '>=', '<=', '<', '>']
 
-    async def _field(self, field):
+    async def _field(self, field, current_value):
         fields = await storage.driver.raw.get_mapping_fields(self.index)
-        return fields
+        if current_value.strip() == "":
+            return fields
+        filtered = [field for field in fields if field.startswith(current_value)]
+        return sorted(filtered)
 
     @staticmethod
-    async def _and_link(field):
+    async def _and_link(field, current_value):
         return ['AND']
 
     @staticmethod
-    async def _or_link(field):
+    async def _or_link(field, current_value):
         return ['OR']
 
     @staticmethod
-    async def _open_bracket(field):
+    async def _open_bracket(field, current_value):
         return ['(']
 
     @staticmethod
-    async def _close_bracket(field):
+    async def _close_bracket(field, current_value):
         return [')']
 
-    async def _value(self, field):
+    async def _value(self, field, current_value):
         print(self.index, field)
         result = await storage.driver.raw.get_unique_field_values(self.index, field)
         return [item.get("key_as_string", item.get("key", None)) for item in result.aggregations("fields").buckets()]
@@ -97,16 +122,30 @@ class Values:
             "VALUE": ("value", "FIELD value")
         }
 
-    async def get(self, token, field) -> List[dict]:
-        values = []
-        if token in self.values:
-            for value in await self.values[token](field):
-                values.append({
+    async def get(self, autocomplete_item: AutocompleteValue, last_field):
+        next_values = []
+        current_values = []
+        next_token = autocomplete_item.next_token.token
+        current_token = autocomplete_item.current_token.token
+        if next_token in self.values:
+            for value in await self.values[next_token](
+                    field=last_field,
+                    current_value=autocomplete_item.current_token.value):
+                next_values.append(HashableDict({
                     "value": value,
-                    "token": self.names[token][0],
-                    "desc": self.names[token][1],
-                })
-        return values
+                    "token": self.names[next_token][0],
+                    "desc": self.names[next_token][1],
+                }))
+            for value in await self.values[current_token](
+                    field=last_field,
+                    current_value=autocomplete_item.current_token.value):
+                current_values.append(HashableDict({
+                    "value": value,
+                    "token": self.names[current_token][0],
+                    "desc": self.names[current_token][1],
+                }))
+
+        return next_values, current_values
 
 
 parser = Lark(schema, parser="lalr")
@@ -117,36 +156,48 @@ class KQLAutocomplete:
         self.last_field = None
         self.last_value = None
         self.last_op = None
+        self.current = None
         self.values = Values(index)
 
     def _get_terms(self, query):
         interactive = parser.parse_interactive(query)
         for token in interactive.iter_parse():  # type: Token
+            self.current = token
+            self.last_token = token.type
             if token.type == "FIELD":
                 self.last_field = token.value
             elif token.type == "OP":
                 self.last_op = token.value
             elif token.type == "VALUE":
                 self.last_value = token.value
+
         interactive.exhaust_lexer()
 
         return interactive.accepts()
 
-    def autocomplete_token(self, query: str):
-        terms = self._get_terms(query)
-        for term in terms:
-
+    def autocomplete_token(self, query: str) -> List[AutocompleteValue]:
+        next_terms = self._get_terms(query)
+        for next_term in next_terms:
             for term_def in parser.terminals:  # type: TerminalDef
 
-                if term_def.name == term:
-                    yield term_def.name
+                if term_def.name == next_term:
+                    yield AutocompleteValue(
+                        next_token=Value(token=term_def.name),
+                        current_token=Value(
+                            token=self.current.type if self.current else term_def.name,
+                            value=self.current.value if self.current else query)
+                    )
 
     async def autocomplete(self, query: str):
-        values = []
-        for token in self.autocomplete_token(query):
-            values += await self.values.get(token, self.last_field)
-
-        return values
+        next_values = []
+        current_values = []
+        for autocomplete_item in self.autocomplete_token(query):
+            _next_values, _current_values = await self.values.get(autocomplete_item, self.last_field)
+            next_values += _next_values
+            current_values += _current_values
+        if query.strip() == "":
+            return next_values
+        return current_values + next_values
 
 
 if __name__ == "__main__":
@@ -155,7 +206,7 @@ if __name__ == "__main__":
 
     async def main():
         ac = KQLAutocomplete(index="event")
-        print(await ac.autocomplete("metadata.time.insert:"))
+        print(await ac.autocomplete(""))
 
 
     asyncio.run(main())
