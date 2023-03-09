5c948eeb3c231ada30cedfd2239d2fd52b18ecb2
risto <risto.kowaczewski@gmail.com>
2023-03-09 12:21:26 +0100

    spaces in IF

--- test/unit/test_if_condition.py
+++ test/unit/test_if_condition.py
@@ -4,12 +4,23 @@ from tracardi.process_engine.tql.condition import Condition
 from tracardi.service.notation.dot_accessor import DotAccessor
 
 
-def test_if_condition_for_spaces():
+def test_if_condition_for_equal():
     async def main():
         dot = DotAccessor(payload={"A": {"a": 1}})
 
         condition = Condition()
         result = await condition.evaluate("payload@A.a==1", dot)
         assert result is True
+
+    asyncio.run(main())
+
+
+def test_if_condition_for_spaces():
+    async def main():
+        dot = DotAccessor(payload={"A": {"a b c": 1}})
+
+        condition = Condition()
+        result = await condition.evaluate("payload@A.\"a b c\" exists", dot)
+        print(result)
 
     asyncio.run(main())
