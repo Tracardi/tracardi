2a1c13e726677557483e135d739116b9c9a7fad9
risto <risto.kowaczewski@gmail.com>
2023-04-19 14:21:28 +0200

    Documentation + profile passing.

--- tracardi/service/tracker.py
+++ tracardi/service/tracker.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import traceback
-from typing import Type, Callable, Coroutine, Any
+from typing import Type, Callable, Coroutine, Any, Optional
 
 from tracardi.domain.named_entity import NamedEntity
 from tracardi.domain.profile import Profile
@@ -11,6 +11,7 @@ from tracardi.exceptions.exception import UnauthorizedException
 from tracardi.domain.payload.tracker_payload import TrackerPayload
 from tracardi.service.logger_manager import save_logs
 from tracardi.service.setup.data.defaults import open_rest_source_bridge
+from tracardi.service.storage.driver import storage
 from tracardi.service.storage.drivers.elastic.operations.console_log import save_console_log
 from tracardi.service.tracker_config import TrackerConfig
 from tracardi.config import memory_cache, tracardi
@@ -115,6 +116,37 @@ class Tracker:
             else:
                 source = await self.validate_source(tracker_payload)
 
+                # Validate tracardi referer. Tracardi referer is a data that has profile_id, session_id. source_id.
+                # It is used to keep the profile between jumps from domain to domain.
+
+                refer_source_id = tracker_payload.get_referer_data('source')
+                if refer_source_id is not None and tracker_payload.has_referred_profile():
+                    # If referred source is different then local web page source saved in JS script
+                    try:
+                        # Check if it is correct source. It will throw exception if incorrect
+                        await self.check_source_id(refer_source_id)
+
+                        referred_profile_id = tracker_payload.get_referer_data('profile')
+
+                        # Check if profile id exists
+                        profile_record = await storage.driver.profile.load_by_id(referred_profile_id)
+
+                        if profile_record is not None:
+
+                            # Invalidate session. It may have wrong profile id
+                            cache.session_cache().delete(tracker_payload.session.id)
+
+                            # Replace the profile id and invalidate session id in tracker payload
+                            tracker_payload.profile.id = referred_profile_id
+                            # tracker_payload.session.id = str(uuid4())
+
+                        else:
+                            logger.warning(f"Referred Tracardi Profile Id {referred_profile_id} is invalid.")
+
+                    except ValueError as e:
+                        logger.warning(f"Referred Tracardi Source Id {refer_source_id} is invalid. "
+                                       f"The following error was returned {str(e)}.")
+
             # Update tracker source with full object
             tracker_payload.source = source
 
@@ -158,6 +190,23 @@ class Tracker:
             self.tracker_config
         )
 
+    async def check_source_id(self, source_id) -> Optional[EventSource]:
+        source = await cache.event_source(event_source_id=source_id, ttl=memory_cache.source_ttl)
+
+        if source is not None:
+
+            if not source.enabled:
+                raise ValueError("Event source disabled.")
+
+            if not source.is_allowed(self.tracker_config.allowed_bridges):
+                raise ValueError(f"This endpoint allows only bridges of "
+                                 f"types {self.tracker_config.allowed_bridges}, but the even source "
+                                 f"`{source.name}`.`{source_id}` has types `{source.type}`. "
+                                 f"Change bridge type in event source `{source.name}` to one that has endpoint type "
+                                 f"{self.tracker_config.allowed_bridges} or call any `{source.type}` endpoint.")
+
+        return source
+
     async def validate_source(self, tracker_payload: TrackerPayload) -> EventSource:
 
         source_id = tracker_payload.source.id
@@ -175,20 +224,10 @@ class Tracker:
                 tags=['internal']
             )
 
-        source = await cache.event_source(event_source_id=source_id, ttl=memory_cache.source_ttl)
+        source = await self.check_source_id(source_id)
 
         if source is None:
             raise ValueError(f"Invalid event source `{source_id}`. Request came from IP: `{ip}` "
                              f"width payload: {tracker_payload}")
-
-        if not source.enabled:
-            raise ValueError("Event source disabled.")
-
-        if not source.is_allowed(self.tracker_config.allowed_bridges):
-            raise ValueError(f"This endpoint allows only bridges of "
-                             f"types {self.tracker_config.allowed_bridges}, but the even source "
-                             f"`{source.name}`.`{source_id}` has types `{source.type}`. "
-                             f"Change bridge type in event source `{source.name}` to one that has endpoint type "
-                             f"{self.tracker_config.allowed_bridges} or call any `{source.type}` endpoint.")
 
         return source
