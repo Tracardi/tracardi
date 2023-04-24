145c6b8c9b41ee7d69c34e9972cc03cccc8885be
risto <risto.kowaczewski@gmail.com>
2023-04-24 14:35:00 +0200

    interests plugins

--- tracardi/service/setup/setup_plugins.py
+++ tracardi/service/setup/setup_plugins.py
@@ -486,6 +486,10 @@ installed_plugins: Dict[str, PluginMetadata] = {
         test=PluginTest(init={'interest': 'abc', 'value': '1.0'},
                         resource=None)
     ),
+    "tracardi.process_engine.action.v1.interest.decrease.plugin": PluginMetadata(
+        test=PluginTest(init={'interest': 'abc', 'value': '1.0'},
+                        resource=None)
+    ),
 
     "tracardi.process_engine.action.v1.converters.data_to_json.plugin": PluginMetadata(
         test=PluginTest(init={'to_json': "{}"},
