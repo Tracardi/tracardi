c415ed6c0c54de3b3eebd6b970d0bf11a0f59d0d
risto <risto.kowaczewski@gmail.com>
2023-07-11 13:19:24 +0200

    Fixed tests

--- test/unit/test_entity_synchronizer.py
+++ test/unit/test_entity_synchronizer.py
@@ -1,15 +1,17 @@
 import asyncio
 
+from tracardi.context import ServerContext, Context
 from tracardi.service.synchronizer import ProfileTracksSynchronizer
 
 
 def test_should_wait_for_unlock():
 
     async def main():
-        es = ProfileTracksSynchronizer(ttl=3)
-        es.lock_entity("1")
-        assert es.is_locked("1")
-        assert not es.is_locked("2")
-        await es.wait_for_unlock("1")
+        with ServerContext(Context(production=False)):
+            es = ProfileTracksSynchronizer(ttl=3)
+            es.lock_entity("1")
+            assert es.is_locked("1")
+            assert not es.is_locked("2")
+            await es.wait_for_unlock("1")
 
     asyncio.run(main())
