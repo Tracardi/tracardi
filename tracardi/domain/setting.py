842320ccbf0013c496c40cd189df00f2d43cb529
risto <risto.kowaczewski@gmail.com>
2023-08-22 12:42:58 +0200

    Metrics mapping change

--- tracardi/domain/setting.py
+++ tracardi/domain/setting.py
@@ -7,8 +7,10 @@ from tracardi.domain.named_entity import NamedEntity
 class Setting(NamedEntity):
     timestamp: Optional[datetime] = None
     description: Optional[str] = ""
+    enabled: Optional[bool] = False
     type: str
     content: dict
+    config: dict
 
     def __init__(self, **data: Any):
         super().__init__(**data)
