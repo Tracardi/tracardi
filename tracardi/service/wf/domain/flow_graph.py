7c435efdd752235751c4277e4c56423b2071b657
risto <risto.kowaczewski@gmail.com>
2023-10-16 19:16:59 +0200

    Test removal of arbitrary object

--- tracardi/service/wf/domain/flow_graph.py
+++ tracardi/service/wf/domain/flow_graph.py
@@ -8,9 +8,10 @@ from pydantic import ConfigDict
 class FlowGraph(NamedEntity):
     description: Optional[str] = None
     flowGraph: Optional[FlowGraphData] = None
-    response: Optional[FlowResponse] = FlowResponse()
+    # response: Optional[FlowResponse] = FlowResponse()
+    response: Optional[dict] = {}
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+    # model_config = ConfigDict(arbitrary_types_allowed=True)
 
     def __init__(self, **data: Any):
         if 'response' in data and isinstance(data['response'], dict):
