26d8bdd8b3078bbf18bbea3f426856f1a35eae1e
risto <risto.kowaczewski@gmail.com>
2023-11-05 22:42:42 +0100

    Fixed is valid_hex

--- tracardi/service/utils/validators.py
+++ tracardi/service/utils/validators.py
@@ -13,5 +13,5 @@ def is_valid_hex(string):
     # Check if the string has exactly 2 characters and they are alphanumeric
     if len(string) == 2 and string.isalnum():
         # Check if both characters are lowercase hexadecimal digits
-        return all(char.islower() and char in '0123456789abcdef' for char in string)
+        return all(char.lower() in '0123456789abcdef' for char in string)
     return False
