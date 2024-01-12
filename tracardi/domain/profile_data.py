a2c993e130ae2fd766db4a3402318812a8a552f5
risto <risto.kowaczewski@gmail.com>
2024-01-12 11:31:40 +0100

    chore(domain): fix type annotation in ProfileData class

--- tracardi/domain/profile_data.py
+++ tracardi/domain/profile_data.py
@@ -163,7 +163,7 @@ class ProfileIdentifier(BaseModel):
     passport: Optional[str] = None
     credit_card: Optional[str] = None
     token: Optional[str] = None
-    coupons: Optional[str | List[str]] = None
+    coupons: Optional[Union[str,List[str]]] = None
 
 
 class ProfileSocialMedia(BaseModel):
