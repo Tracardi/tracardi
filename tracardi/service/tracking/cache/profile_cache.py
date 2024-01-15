5fce0a2d68c97cd2d467235881debae83f8c15ef
risto <risto.kowaczewski@gmail.com>
2024-01-15 13:16:39 +0100

    feat(docker): Update APM image to version 0.8.2-rc4
    chore(profile_cache): Update import statement for async_mutex

--- tracardi/service/tracking/cache/profile_cache.py
+++ tracardi/service/tracking/cache/profile_cache.py
@@ -10,7 +10,7 @@ from tracardi.service.storage.redis.cache import RedisCache
 from tracardi.service.storage.redis.collections import Collection
 from tracardi.service.storage.redis_client import RedisClient
 from tracardi.service.tracking.cache.prefix import get_cache_prefix
-from tracardi.service.tracking.locking import Lock
+from tracardi.service.tracking.locking import Lock, async_mutex
 from tracardi.service.utils.getters import get_entity_id
 from tracardi.domain.profile import Profile
 from tracardi.service.merging.profile_merger import merge_profiles
@@ -133,5 +133,5 @@ def merge_with_cache_and_save_profile(profile: Profile, context: Context):
 async def lock_merge_with_cache_and_save_profile(profile: Profile, context: Context, lock_name=None):
     profile_key = Lock.get_key(Collection.lock_tracker, "profile", get_entity_id(profile))
     profile_lock = Lock(_redis, profile_key, default_lock_ttl=3)
-    async with mutex(profile_lock, name=lock_name):
+    async with async_mutex(profile_lock, name=lock_name):
         return merge_with_cache_and_save_profile(profile, context)
