b46f6860638ace6c6d93903b0666a76b5ec9dada
risto <risto.kowaczewski@gmail.com>
2024-01-20 01:28:13 +0100

    feat(storage): Add MySqlQueryResult class and MysqlQuery class
    This commit adds the `MySqlQueryResult` class, which standardizes the output
    from `MysqlQuery`, and the `MysqlQuery` class, which provides methods for
    performing various SQL operations. The `_select_clause` method in the
    `MysqlQuery` class is responsible for generating the SELECT query.
    
    The `update` method in the `MysqlQuery` class allows for updating records in
    a specified table, while the `delete` method enables the deletion of records
    from a table. The `select` method retrieves records from a table based on
    specified criteria.
    
    This commit also includes necessary imports and type hints for improved code
    readability and maintainability.

---
+++ tracardi/service/storage/mysql/query_service.py
@@ -0,0 +1,108 @@
+from tracardi.context import get_context
+from typing import Type, Any, Callable, Optional
+
+from tracardi.service.storage.mysql.schema.table import Base
+from sqlalchemy import Column, Select, update, delete, ChunkedIteratorResult
+from sqlalchemy.future import select
+
+class MySqlQueryResult:
+
+    """
+    Standardizes the output form MysqlQuery
+    """
+
+    def __init__(self, data):
+        self.data = data
+
+    def all(self):
+        if isinstance(self.data, ChunkedIteratorResult):
+            return self.data.scalars().all()
+        else:
+            return self.data
+
+    def one_or_none(self):
+        if isinstance(self.data, ChunkedIteratorResult):
+            return self.data.scalars().one_or_none()
+        else:
+            return self.data[0] if self.data else None
+
+
+    def one(self):
+        if isinstance(self.data, ChunkedIteratorResult):
+            return self.data.scalars().one()
+        else:
+            return self.data[0]
+
+    def empty(self) -> bool:
+        if isinstance(self.data, ChunkedIteratorResult):
+            return self.data.scalars() is None
+        else:
+            return self.data
+
+class MysqlQuery:
+
+    def __init__(self, session):
+        self.session = session
+
+    @staticmethod
+    def _select_clause(table: Type[Base],
+                       columns=None,
+                       where: Optional[Callable] = None,
+                       order_by: Column = None,
+                       limit: int = None,
+                       offset: int = None,
+                       distinct: bool = False) -> Select[Any]:
+
+        if columns is not None:
+            _select = select(*columns)
+        else:
+            _select = select(table)
+
+        if distinct:
+            _select = _select.distinct()
+
+        if where is not None:
+            _select = _select.where(where())
+
+        if order_by is not None:
+            _select = _select.order_by(order_by)
+
+        if limit:
+            _select = _select.limit(limit)
+
+            if offset:
+                _select = _select.offset(offset)
+
+        return _select
+
+    async def update(self, table: Type[Base], new_data, where: Optional[Callable] = None):
+        stmt = (
+            update(table)
+            .where(where())
+            .values(**new_data)
+        )
+        return await self.session.execute(stmt)
+
+    async def delete(self, table: Type[Base], where: Optional[Callable] = None):
+        return await self.session.execute(
+            delete(table).where(where())
+        )
+
+    async def select(self, table: Type[Base],
+                     columns=None,
+                     where: Optional[Callable] = None,
+                     order_by: Column = None,
+                     limit: int = None,
+                     offset: int = None,
+                     distinct: bool = False) -> MySqlQueryResult:
+        context = get_context()
+
+        query = self._select_clause(table,
+                                    columns,
+                                    where,
+                                    order_by,
+                                    limit,
+                                    offset,
+                                    distinct)
+
+        return MySqlQueryResult(await self.session.execute(query))
