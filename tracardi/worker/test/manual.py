b892539e0b967bdc49b66ddf77737b849dca3bad
risto <risto.kowaczewski@gmail.com>
2024-03-02 11:56:43 +0100

    Bump version to 0.9.0-rc3 in config and flow domain.

--- tracardi/worker/test/manual.py
+++ tracardi/worker/test/manual.py
@@ -31,7 +31,7 @@ async def main():
         },
         "params": {
             "mysql": "user",
-            "version": "0.9.0-rc2"
+            "version": "0.9.0-rc3"
         },
         "worker": "copy_to_mysql",
         "asynchronous": True
