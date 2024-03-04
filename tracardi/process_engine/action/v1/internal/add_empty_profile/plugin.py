5911914b2b3227b43f84513ecd69147c12354ebb
risto <risto.kowaczewski@gmail.com>
2024-03-04 20:38:50 +0100

    refactor: remove print statement and log empty profile metadata warning

--- tracardi/process_engine/action/v1/internal/add_empty_profile/plugin.py
+++ tracardi/process_engine/action/v1/internal/add_empty_profile/plugin.py
@@ -36,7 +36,7 @@ class AddEmptyProfileAction(ActionRunner):
                     )
                 )
             ),
-            operation=Operation(update=True)
+            operation=Operation(new=True, update=True)
         )
 
         self.event.profile = profile
@@ -59,14 +59,15 @@ class AddEmptyProfileAction(ActionRunner):
             id=str(uuid4()),
             profile=PrimaryEntity(id=profile.id),
             metadata=SessionMetadata(time=SessionTime()),
-            operation=Operation(update=True)
+            operation=Operation(new=True, update=True)
         )
 
         # todo set session in tracker payload
 
         if self.session is not None:
-            self.console.warning(f"Old session {self.session.id} was replaced by new session {session.id}. "
-                                 f"Replacing session is not a good practice if you already have a session.")
+            self.console.warning(
+                f"Old session {self.session.id} was replaced by new session {session.id}. "
+                f"Replacing session is not a good practice if you already have a session.")
 
         self.session = session
 
