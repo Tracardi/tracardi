9a3cff9c1a30f6cad23fab05f2c7912f67da14a2
risto <risto.kowaczewski@gmail.com>
2024-03-25 11:24:53 +0100

    Update boto3 version to 1.34.41.

--- tracardi/process_engine/action/v1/connectors/aws/s3/upload_segments/plugin.py
+++ tracardi/process_engine/action/v1/connectors/aws/s3/upload_segments/plugin.py
@@ -1,4 +1,5 @@
 import json
+# from aiobotocore.session import get_session
 import boto3
 import tempfile
 import os
@@ -21,6 +22,11 @@ class S3SegmentsUploaderPlugin(ActionRunner):
         self.config = validate(config)
 
     async def run(self, payload: dict, in_edge=None):
+        # session = get_session()
+        # async with session.create_client('s3',
+        #                                  aws_secret_access_key=self.config.aws_secret_access_key,
+        #                                  aws_access_key_id=self.config.aws_access_key_id) as s3:
+
         s3 = boto3.client(
             's3',
             aws_access_key_id=self.config.aws_access_key_id,
