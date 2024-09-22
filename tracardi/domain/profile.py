129ba4f8f4269e691055ba81116205af37af9e81
risto <risto.kowaczewski@gmail.com>
2024-09-22 10:04:20 +0200

    chore(profile): remove commented out set_auto_merge_fields method
    log(main): add APM status logging on startup

--- tracardi/domain/profile.py
+++ tracardi/domain/profile.py
@@ -285,12 +285,6 @@ class Profile(PrimaryEntity):
 
     # --------------- ID Hashing -----------------------
 
-    # def set_auto_merge_fields(self, auto_merge_ids: set):
-    #     if 'auto_merge' not in self.aux or not isinstance(self.aux['auto_merge'], list):
-    #         self.aux['auto_merge'] = list(auto_merge_ids)
-    #     else:
-    #         self.aux['auto_merge'] = list(set(self.aux['auto_merge']).union(auto_merge_ids))
-
     def hash_all_allowed_pii_as_ids(self) -> bool:
 
         """ Used for creating hashed IDS """
