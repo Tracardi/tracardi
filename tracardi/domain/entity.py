fb58a35cb4c083f67989d07d32073bb59c3d00fb
risto <risto.kowaczewski@gmail.com>
2024-11-12 09:17:50 +0100

    fix: add warning log if no _changes property is found

--- tracardi/domain/entity.py
+++ tracardi/domain/entity.py
@@ -193,6 +193,8 @@ class FlatEntity(Dotty):
                               event_type,
                               ignore=('metadata.fields', 'operation')
                               )
+        else:
+            logger.warning(f"No _changes property.")
         super().__setitem__(key, value)
 
     def override(self, key, value):
