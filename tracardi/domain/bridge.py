d158ab1febcb43e5ef480f8f018cf27f7cb749b2
risto <risto.kowaczewski@gmail.com>
2024-12-07 19:50:54 +0100

    fix(tenant-manager): Remove localhost check for tenant derivation
    refactor(bridge): Add method to derive ID in tenant context
    
    feat(bridge-service): Integrate tenant-specific ID generation
    
    - Remove check for localhost in tenant derivation logic
    - Add method in Bridge class to derive IDs in tenant context
    - Use context-driven ID generation during bridge bootstrap and reinstall

--- tracardi/domain/bridge.py
+++ tracardi/domain/bridge.py
@@ -1,7 +1,9 @@
+from hashlib import md5
 from typing import Optional
 
 from tracardi.domain.named_entity import NamedEntity
 from tracardi.service.plugin.domain.register import Form
+from tracardi.service.utils.hasher import uuid4_from_md5
 
 
 class Bridge(NamedEntity):
@@ -10,3 +12,7 @@ class Bridge(NamedEntity):
     config: Optional[dict] = {}
     form: Optional[Form] = None
     manual: Optional[str] = None
+
+    def get_id_in_context_of_tenant(self, context) -> str:
+        id_in_context_of_tenant = md5(f"{self.id}-{context.tenant}".encode()).hexdigest()
+        return uuid4_from_md5(id_in_context_of_tenant)
