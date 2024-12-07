39b1990454bedb6f8f426b16c15b7bc5e170ff9a
risto <risto.kowaczewski@gmail.com>
2024-12-07 16:58:23 +0100

    feat(logging): update default logging settings
    Change log stack trace format to JSON and extend log levels to
    include WARNING and INFO. Adjust default log bulk size to 1.

--- tracardi/logging.py
+++ tracardi/logging.py
@@ -2,6 +2,6 @@ import os
 
 from tracardi.service.utils.environment import get_env_as_int
 
-log_stack_trace_as = os.environ.get('LOG_STACK_TRACE_AS', 'string')
-log_stack_trace_for = os.environ.get('LOG_STACK_TRACE_AS', 'CRITICAL,ERROR').split(',')
-log_bulk_size = get_env_as_int('LOG_BULK_SIZE', 500)
+log_stack_trace_as = os.environ.get('LOG_STACK_TRACE_AS', 'json')
+log_stack_trace_for = os.environ.get('LOG_STACK_TRACE_AS', 'CRITICAL,ERROR,WARNING,INFO').split(',')
+log_bulk_size = get_env_as_int('LOG_BULK_SIZE', 1)
