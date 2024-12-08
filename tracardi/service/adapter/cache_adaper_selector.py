ae5676b39498bfebe6c1259b17726441bfae7c68
risto <risto.kowaczewski@gmail.com>
2024-12-08 14:21:59 +0100

    refactor: conditionally import cache adapters based on configuration
    Adjust the import statements to load cache adapters only when the
    specified cache adapter variable matches 'redis' or 'upstash'. This
    reduces unnecessary imports and optimizes the module's performance.

--- tracardi/service/adapter/cache_adaper_selector.py
+++ tracardi/service/adapter/cache_adaper_selector.py
@@ -3,17 +3,20 @@ from tracardi.service.adapter.cache.cache_protocol import CacheProtocol
 from tracardi.service.adapter.cache.hcache_protocol import HCacheProtocol
 from tracardi.service.adapter.cache.member_cache_protocol import MemberCacheProtocol
 from tracardi.service.adapter.cache.pubsub_protocol import PubSubProtocol
-from tracardi.service.adapter.cache.redis.redis_cache_adapter import RedisCacheAdapter
-from tracardi.service.adapter.cache.redis.redis_hcache_adapter import RedisHCacheAdapter
-from tracardi.service.adapter.cache.redis.redis_members_adapter import RedisMembersCacheAdapter
-from tracardi.service.adapter.cache.redis.redis_pubsub_adapter import RedisPubSubAdapter
-from tracardi.service.adapter.cache.upstash.upstash_redis_cache_adapter import UpStashRedisCacheAdapter
-from tracardi.service.adapter.cache.upstash.upstash_redis_hcache_adapter import UpStashRedisHCacheAdapter
-from tracardi.service.adapter.cache.upstash.upstash_redis_members_adapter import UpStashRedisMembersCacheAdapter
-from tracardi.service.adapter.cache.upstash.upstash_redis_pubsub_adapter import UpStashRedisPubSubAdapter
 from tracardi.service.decorators.run_once import run_once
 
 _cache_adapter_var = tracardi.cache_adapter
+
+if _cache_adapter_var == 'redis':
+    from tracardi.service.adapter.cache.redis.redis_cache_adapter import RedisCacheAdapter
+    from tracardi.service.adapter.cache.redis.redis_hcache_adapter import RedisHCacheAdapter
+    from tracardi.service.adapter.cache.redis.redis_members_adapter import RedisMembersCacheAdapter
+    from tracardi.service.adapter.cache.redis.redis_pubsub_adapter import RedisPubSubAdapter
+elif _cache_adapter_var == 'upstash':
+    from tracardi.service.adapter.cache.upstash.upstash_redis_cache_adapter import UpStashRedisCacheAdapter
+    from tracardi.service.adapter.cache.upstash.upstash_redis_hcache_adapter import UpStashRedisHCacheAdapter
+    from tracardi.service.adapter.cache.upstash.upstash_redis_members_adapter import UpStashRedisMembersCacheAdapter
+    from tracardi.service.adapter.cache.upstash.upstash_redis_pubsub_adapter import UpStashRedisPubSubAdapter
 
 
 @run_once
