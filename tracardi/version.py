203aef73ebcf428721cf3b53a51958ca67edc0fe
risto <risto.kowaczewski@gmail.com>
2024-12-10 12:31:50 +0100

    chore: update version to 1.2.x in version.py

--- tracardi/version.py
+++ tracardi/version.py
@@ -2,7 +2,7 @@ import os
 from tracardi.domain.version import Version
 from tracardi.service.utils.environment import get_env_as_bool
 
-VERSION = os.environ.get('_DEBUG_VERSION', '1.1.x')
+VERSION = os.environ.get('_DEBUG_VERSION', '1.2.x')
 TENANT_NAME = os.environ.get('TENANT_NAME', None)
 PRODUCTION = os.environ.get('PRODUCTION', 'no').lower() == 'yes'
 MULTI_TENANT = get_env_as_bool('MULTI_TENANT', "no")
