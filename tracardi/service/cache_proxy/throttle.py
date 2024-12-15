a92d9a1725ee8847b9647f6c9f8c4725511129e1
risto <risto.kowaczewski@gmail.com>
2024-12-15 13:02:28 +0100

    ``` refactor(throttle): enhance throttling mechanism
    Refactor the Throttler class to include a resource identifier
    for improved throttling. Introduce `is_throttled` method for
    checking throttling status. Modify `call` method to accept a
    namespace parameter and use the resource identifier for
    managing last run time.
    ```

--- tracardi/service/cache_proxy/throttle.py
+++ tracardi/service/cache_proxy/throttle.py
@@ -8,32 +8,26 @@ class Throttler:
     cached_results = defaultdict(dict)
     last_run_time = {}
 
-    def __init__(self, interval: float):
-        """
-        Initialize the throttler with a specific interval.
-        :param interval: Minimum time (in seconds) between consecutive executions.
-        """
+    def __init__(self, resource: str, interval: float):
+        self._resource = resource
         self.interval = interval
 
-    async def call(self, func: Callable[..., Awaitable[Any]], *args, **kwargs) -> Any:
-        """
-        Call the provided function with throttling.
-        :param func: Function to be throttled.
-        :param args: Positional arguments for the function.
-        :param kwargs: Keyword arguments for the function.
-        :return: Cached or newly computed result of the function.
-        """
+    def is_throttled(self, current_time):
+        return self._resource in Throttler.last_run_time and current_time - Throttler.last_run_time[self._resource] <= self.interval
+
+    async def call(self, namespace:str, func: Callable[..., Awaitable[Any]], *args, **kwargs) -> Any:
+
         # Create a unique key based on function arguments
         key = (func, args, frozenset(kwargs.items()))
         current_time = time.time()
         # Check if the function can be executed
-        if key not in Throttler.last_run_time or current_time - Throttler.last_run_time[key] >= self.interval:
+        if self.is_throttled(current_time):
+            result = Throttler.cached_results[key]
+        else:
             # Run the function and store the result
             result = await func(*args, **kwargs)
             Throttler.cached_results[key] = result
-            Throttler.last_run_time[key] = current_time
-        else:
-            result = Throttler.cached_results[key]
+            Throttler.last_run_time[self._resource] = current_time
 
         return result
 
