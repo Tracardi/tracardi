dfc25d1b53487664eb1e476d5818e76d349db692
risto <risto.kowaczewski@gmail.com>
2024-12-16 12:34:00 +0100

    refactor(throttle): improve throttler caching logic
    Refactor the Throttler class to enhance its caching logic. Add a new
    async method `_run` for executing and caching function results. Introduce
    `not_cached` method to check cache presence. Refactor `__call__` method
    to utilize these enhancements, ensuring non-cached executions when
    throttled.

--- tracardi/service/cache_proxy/throttle.py
+++ tracardi/service/cache_proxy/throttle.py
@@ -2,16 +2,24 @@ import time
 from collections import defaultdict
 from typing import Callable, Any, Awaitable
 
-
 class Throttler:
 
     cached_results = defaultdict(dict)
 
-    def __init__(self, resource: str, interval: float, return_cache_when_throttled=True):
+    def __init__(self, resource: str, interval: float, return_cache_when_throttled:bool=True):
         self._return_cache_when_throttled = return_cache_when_throttled
         self._resource = resource
         self.interval = interval
         self._last_run_time = 0
+
+    async def _run(self, key, current_time, func, args, kwargs):
+        result = await func(*args, **kwargs)
+        Throttler.cached_results[key] = result
+        self._last_run_time = current_time
+        return result
+
+    def not_cached(self, key):
+        return key not in Throttler.cached_results
 
     def is_throttled(self, current_time):
         return current_time - self._last_run_time <= self.interval
@@ -21,14 +29,14 @@ class Throttler:
         # Create a unique key based on function arguments
         key = (func, args, frozenset(kwargs.items()))
         current_time = time.time()
-        # Check if the function can be executed
-        if self.is_throttled(current_time):
-            result = Throttler.cached_results[key]
-        else:
-            # Run the function and store the result
-            result = await func(*args, **kwargs)
-            Throttler.cached_results[key] = result
-            self._last_run_time = current_time
 
-        return result
+        # Check if the function can be executed
+        if not self.is_throttled(current_time) or self.not_cached(key):
+            return await self._run(key, current_time, func, args, kwargs)
+
+        # Throttled
+        if not self._return_cache_when_throttled:
+            return None
+
+        return Throttler.cached_results.get(key, None)
 
