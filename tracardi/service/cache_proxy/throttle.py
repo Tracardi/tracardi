deaa0454caa476afc6c5478df607e636acaa83a0
risto <risto.kowaczewski@gmail.com>
2024-12-16 10:43:59 +0100

    feat(throttle): refactor Throttler class to improve caching logic
    Refactor the Throttler class by removing the last_run_time class
    variable, and replacing it with an instance variable _last_run_time.
    Add a new parameter return_cache_when_throttled to control cache
    return behavior. Simplify the is_throttled method using instance
    variable.

--- tracardi/service/cache_proxy/throttle.py
+++ tracardi/service/cache_proxy/throttle.py
@@ -6,16 +6,17 @@ from typing import Callable, Any, Awaitable
 class Throttler:
 
     cached_results = defaultdict(dict)
-    last_run_time = {}
 
-    def __init__(self, resource: str, interval: float):
+    def __init__(self, resource: str, interval: float, return_cache_when_throttled=True):
+        self._return_cache_when_throttled = return_cache_when_throttled
         self._resource = resource
         self.interval = interval
+        self._last_run_time = 0
 
     def is_throttled(self, current_time):
-        return self._resource in Throttler.last_run_time and current_time - Throttler.last_run_time[self._resource] <= self.interval
+        return current_time - self._last_run_time <= self.interval
 
-    async def call(self, namespace:str, func: Callable[..., Awaitable[Any]], *args, **kwargs) -> Any:
+    async def call(self, func: Callable[..., Awaitable[Any]], *args, **kwargs) -> Any:
 
         # Create a unique key based on function arguments
         key = (func, args, frozenset(kwargs.items()))
@@ -27,7 +28,7 @@ class Throttler:
             # Run the function and store the result
             result = await func(*args, **kwargs)
             Throttler.cached_results[key] = result
-            Throttler.last_run_time[self._resource] = current_time
+            self._last_run_time = current_time
 
         return result
 
