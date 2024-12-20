e757c3e72719690af757677a407ecb49b588dc02
risto <risto.kowaczewski@gmail.com>
2024-12-20 11:12:54 +0100

    proxy cache

--- tracardi/service/tracker.py
+++ tracardi/service/tracker.py
@@ -123,7 +123,7 @@ class Tracker:
         finally:
             global _measures
             context.profiler.measure('tracker-ends')
-            if len(_measures) > 100:
+            if len(_measures) > 5000:
 
                 if License.has_license():
                     # Calculate and print statistics
