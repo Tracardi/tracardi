d9df0a8cc308c0bc3df632f5b03e0b5c6e323c24
risto <risto.kowaczewski@gmail.com>
2024-12-25 12:59:47 +0100

    Cache info

--- tracardi/config.py
+++ tracardi/config.py
@@ -27,23 +27,23 @@ def _get_random_value(value) -> float:
 
 class MemoryCacheConfig:
     def __init__(self):
-        _default_ttl = 180  # Check every 60 sec
+        self.default_ttl = 180  # Check every 60 sec
 
         self.timeout_sql_query_in = 1
         self.max_one_cache_fill_every = .5
         self.event_to_profile_coping_ttl = _get_random_value(
-            get_env_as_int('EVENT_TO_PROFILE_COPY_CACHE_TTL', _default_ttl))
-        self.source_ttl = _get_random_value(get_env_as_int('SOURCE_CACHE_TTL', _default_ttl))
-        self.event_validation_cache_ttl = _get_random_value(get_env_as_int('EVENT_VALIDATION_CACHE_TTL', _default_ttl))
-        self.data_compliance_cache_ttl = _get_random_value(get_env_as_int('DATA_COMPLIANCE_CACHE_TTL', _default_ttl))
-        self.event_mapping_cache_ttl = _get_random_value(get_env_as_int('EVENT_METADATA_CACHE_TTL', _default_ttl))
-        self.trigger_rule_cache_ttl = _get_random_value(get_env_as_int('TRIGGER_RULE_CACHE_TTL', _default_ttl))
+            get_env_as_int('EVENT_TO_PROFILE_COPY_CACHE_TTL', self.default_ttl))
+        self.source_ttl = _get_random_value(get_env_as_int('SOURCE_CACHE_TTL', self.default_ttl))
+        self.event_validation_cache_ttl = _get_random_value(get_env_as_int('EVENT_VALIDATION_CACHE_TTL', self.default_ttl))
+        self.data_compliance_cache_ttl = _get_random_value(get_env_as_int('DATA_COMPLIANCE_CACHE_TTL', self.default_ttl))
+        self.event_mapping_cache_ttl = _get_random_value(get_env_as_int('EVENT_METADATA_CACHE_TTL', self.default_ttl))
+        self.trigger_rule_cache_ttl = _get_random_value(get_env_as_int('TRIGGER_RULE_CACHE_TTL', self.default_ttl))
         self.event_destination_cache_ttl = _get_random_value(get_env_as_int('EVENT_DESTINATION_CACHE_TTL', 180))
         self.profile_destination_cache_ttl = _get_random_value(get_env_as_int('PROFILE_DESTINATION_CACHE_TTL', 180))
-        self.event_reshaping_cache_ttl = _get_random_value(get_env_as_int('EVENT_RESHAPING_CACHE_TTL', _default_ttl))
+        self.event_reshaping_cache_ttl = _get_random_value(get_env_as_int('EVENT_RESHAPING_CACHE_TTL', self.default_ttl))
         self.identification_points_cache_ttl = _get_random_value(
-            get_env_as_int('IDENTIFICATION_POINTS_CACHE_TTL', _default_ttl))
-        self.resource_load_cache_ttl = _get_random_value(get_env_as_int('RESOURCE_LOAD_CACHE_TTL', _default_ttl))
+            get_env_as_int('IDENTIFICATION_POINTS_CACHE_TTL', self.default_ttl))
+        self.resource_load_cache_ttl = _get_random_value(get_env_as_int('RESOURCE_LOAD_CACHE_TTL', self.default_ttl))
 
 
 class MysqlConfig:
