fa176449fa0641f87fe389709364be5f46b4877e
risto <risto.kowaczewski@gmail.com>
2025-01-02 16:50:48 +0100

    Adapter error fix

--- tracardi/service/adapter/bigdata/adapter_selector.py
+++ tracardi/service/adapter/bigdata/adapter_selector.py
@@ -1,14 +1,13 @@
+from tracardi.config import tracardi
 from tracardi.service.adapter.bigdata.elastic.elastic_adapter import ElasticAdapter
 from tracardi.service.adapter.bigdata.elastic.elastic_apm_adapter import ElasticApmAdapter
-from com_tracardi.config import com_tracardi_settings
 from tracardi.service.adapter.bigdata.elastic.elastic_collector_adapter import ElasticCollectorAdapter
 from tracardi.service.adapter.bigdata.elastic.elastic_log_adapter import ElasticLogAdapter
 from tracardi.service.adapter.bigdata.elastic.elastic_gui_search_adapter import ElasticSearchAdapter
 from tracardi.service.adapter.bigdata.elastic.elstic_analitics_adapter import ElasticAnalyticsAdapter
-
 from tracardi.common.decorator.run_once import run_once
 
-_big_data_adapter_var = com_tracardi_settings.big_data_adapter
+_big_data_adapter_var = tracardi.big_data_adapter
 
 
 @run_once
