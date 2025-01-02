7db53e08b1929080149d4cafb8c39a947b562b7b
risto <risto.kowaczewski@gmail.com>
2025-01-02 01:00:23 +0100

    feat: upgrade python dependencies (#1.2.x)
    Update Dockerfiles in both `com-tracardi` and `tracardi-api` repositories to
    include python3.10-dev and python3.11-dev respectively. Upgrade asyncpg
    library version from 0.26.0 to 0.30.0 in `tracardi` repository's `setup.py` and
    `requirements.txt` files. Remove unused `kasuj.json` file from `tracardi`
    repository.

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'geoip2==4.2.0',
         'aiomysql==0.1.1',
         'kombu==5.2.4',
-        'asyncpg==0.26.0',
+        'asyncpg==0.30.0',
         'aiobotocore~=2.12.1',
         'google-api-python-client == 2.33.0',
         'google_auth_oauthlib == 0.4.6',
