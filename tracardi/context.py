f34a067109faf663cea458151b7fc4cfc8167f47
risto <risto.kowaczewski@gmail.com>
2025-01-09 09:46:10 +0100

    Refactor `TransportContext` usage and cleanup logging.
    Replaced `to_transport_context` calls with `TransportContext.build` for consistency and removed redundant `get_logger` instances across modules. Additionally, addressed minor logging and import cleanup to enhance maintainability.

--- tracardi/context.py
+++ tracardi/context.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 from typing import Optional, Any
 from uuid import uuid4
 
-from defer.model.transport_context import TransportContext
 from tracardi.version import version as system_version
 from tracardi.domain.user import User
 from tracardi.common.singleton import Singleton
@@ -156,9 +155,6 @@ class Context:
             host=context.get('host'),
             version=context.get('version')
         )
-
-    def to_transport_context(self) -> TransportContext:
-        return TransportContext(tenant=self.tenant, production=self.production, properties={})
 
 class ContextManager(metaclass=Singleton):
 
