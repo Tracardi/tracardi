97bc084ee1171599a8dd30c5b78f4be8ff900343
risto <risto.kowaczewski@gmail.com>
2025-01-28 20:55:32 +0100

    Initialize `bd_elastic_adapter` during module load.
    This change ensures that `bd_elastic_adapter` is instantiated immediately when the module is loaded, aligning it with the initialization of other adapters. It prevents potential runtime errors due to missing initialization.

--- test/manual/_test_cache_validator.py
+++ test/manual/_test_cache_validator.py
@@ -1,17 +1,16 @@
-# Example usage:
-from tracardi.context import ServerContext, Context
-from tracardi.service.cache_change_tagger.change_tagger import validate_cache_on_load, invalidate_cache_on_update
-
-
+# # Example usage:
 # from tracardi.context import ServerContext, Context
-def my_function():
-    print("Executing my_function...")
-    # Simulating some logic
-    return "Success!"
-
-
-# Test the decorators
-if __name__ == "__main__":
-    with ServerContext(Context(production=False)):
-        with invalidate_cache_on_update(database="my_db", table="my_table"):
-            print(my_function())  # Should execute successfully and trigger table_changed
+#
+#
+# # from tracardi.context import ServerContext, Context
+# def my_function():
+#     print("Executing my_function...")
+#     # Simulating some logic
+#     return "Success!"
+#
+#
+# # Test the decorators
+# if __name__ == "__main__":
+#     with ServerContext(Context(production=False)):
+#         with invalidate_cache_on_update(database="my_db", table="my_table"):
+#             print(my_function())  # Should execute successfully and trigger table_changed
