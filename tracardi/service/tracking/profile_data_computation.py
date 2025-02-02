42b13592d24b265887e5b6f010b275d9604b55b8
risto <risto.kowaczewski@gmail.com>
2025-02-02 16:26:08 +0100

    Refactoring.

--- tracardi/service/tracking/profile_data_computation.py
+++ tracardi/service/tracking/profile_data_computation.py
@@ -101,7 +101,7 @@ async def _custom_event_to_profile_mapping(custom_mapping_schemas,
                                            flat_event: FlatEvent,
                                            session: Session) -> AsyncGenerator[FieldChange, None]:
     if custom_mapping_schemas is not None and len(custom_mapping_schemas) > 0:
-
+        print(1, flat_event['properties'])
         event_create_timestamp = flat_event.metadata_time.create.timestamp()
         for custom_mapping_schema in custom_mapping_schemas:
 
