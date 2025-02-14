a87f1aece09ca4023b752b911a582a07ef0aac60
risto <risto.kowaczewski@gmail.com>
2025-02-14 19:32:09 +0100

    feat(ee): Refactor Starrocks adapters and improve mapping (1.2.y)
    Relocate Starrocks adapters from 'os' to 'ee' directory and rename them. Update the corresponding tests. Improve the mapping of profile records by using DotDict from Generic DotDict package, and remove unnecessary comments. Rename the schema files and add new '__init__.py' files to make the codebase more organized. This also enhances the functionality of the Starrocks adapters. Introduce 'order.py' and 'limit.py' in 'srd/domain/' directory, which contain classes defining orders and limits for queries.

--- tracardi/install/service/installation.py
+++ tracardi/install/service/installation.py
@@ -3,13 +3,19 @@ from tracardi.domain.credentials import Credentials
 from tracardi.common.logging.log_handler import get_installation_logger
 from tracardi.service.dependency.adapters.big_data_adapter import *
 from tracardi.service.dependency.adapters.mysql import md_install_adapter
+from tracardi.service.plugin.plugin_install import install_default_plugins
 
 logger = get_installation_logger(__name__)
 
 
 async def install_system(credentials: Credentials):
     admin = await md_install_adapter.install_mysql_database(credentials, version=tracardi.version)
+    installed_plugins = await install_default_plugins()
+
     staging_install_result, production_install_result = await bd_install_adapter.install_big_data_database(credentials)
+
+    staging_install_result['plugins'] = installed_plugins
+    production_install_result['plugins'] = installed_plugins
 
     staging_install_result['admin'] = admin
     return staging_install_result, production_install_result
