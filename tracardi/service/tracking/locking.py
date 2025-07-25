af1daef8f4c8ca4d921b63b07662d3350a311b90
risto <risto.kowaczewski@gmail.com>
2025-07-25 12:55:02 +0200

    Update expiration date in main.py and adjust grpc dependencies.

--- tracardi/service/tracking/locking.py
+++ tracardi/service/tracking/locking.py
@@ -16,6 +16,7 @@ EXPIRED = 2
 RELEASED = 3
 DONE_WAITING = 4
 
+
 class Lock:
 
     def __init__(self, redis, key, default_lock_ttl: float):
@@ -273,59 +274,6 @@ class AsyncGlobalMutexLock(_GlobalMutexLock):
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         self._exit(exc_type)
 
-# TODO remove after 2025-03-01
-# class AsyncProfileMutex(_GlobalMutexLock):
-#
-#     def __init__(self, profile_id: str, name: str, break_after_time: Union[int, float] = None,
-#                  raise_error_when_locked: bool = False):
-#         self.profile_id = profile_id
-#         profile_key = Lock.get_key(Collection.lock_tracker, "profile", profile_id)
-#         profile_lock = Lock(_redis, profile_key, default_lock_ttl=3)
-#         if profile_lock.is_locked() and raise_error_when_locked:
-#             raise BlockingIOError(
-#                 f"Profile {profile_lock.key} is locked. Currently locked by (Running process): "
-#                 f"{profile_lock.get_locked_inside()}, Knocking consumer (Waiting process): {name}")
-#
-#         super().__init__(profile_lock, name, break_after_time)
-#
-#     async def _keep_locked_for(self) -> 'Lock':
-#
-#         while True:
-#             _now = time.time()
-#             if self._lock.is_locked():  # Key exists, when expires it will be unlocked
-#
-#                 lock_time = self._get_lock_time()
-#
-#                 # Check if there is a time to break the lock
-#                 _broke, _time_to_break = self._check_if_it_is_time(lock_time, grace_period=self._break_after_time)
-#                 if _broke:  # Time is up
-#                     # We are fed up waiting
-#                     logger.info(
-#                         f"Lock {self._lock.key} breaks. Currently locked by (Running process): {self._lock.get_locked_inside()}, Knocking consumer (Waiting process): {self._name}")
-#                     self._lock.break_in()  # Still locked but break in marked BROKE
-#                     return self._lock
-#
-#                 logger.info(
-#                     f"Suppressing execution of {self._lock.key}. Process {self._lock.get_locked_inside()} is using resource."
-#                     f"Expires in {self._lock.ttl}s. Waiting no longer then {_time_to_break}s then skipping execution."
-#                 )
-#
-#                 await asyncio.sleep(self._wait)
-#
-#                 continue
-#             break
-#
-#         self._lock.lock(self._name)
-#
-#     async def __aenter__(self) -> Optional[Profile]:
-#         if self._lock.key is None:
-#             return await load_profile(self.profile_id)
-#         await self._keep_locked_for()
-#         return await load_profile(self.profile_id)
-#
-#     async def __aexit__(self, exc_type, exc_val, exc_tb):
-#         self._exit(exc_type)
-
 
 def mutex(lock: Lock, name: str, break_after_time: Union[int, float] = None, raise_error_when_locked: bool = False):
     if lock.is_locked() and raise_error_when_locked:
@@ -342,4 +290,3 @@ def async_mutex(lock: Lock,
         raise BlockingIOError(
             f"Resource {lock.key} is locked. Currently locked by (Running process): {lock.get_locked_inside()}, Waiting consumer: {name}")
     return AsyncGlobalMutexLock(lock, name, break_after_time)
-
